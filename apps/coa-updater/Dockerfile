47d2a189fc8efbd075be3abce1dcda08e59df386
Bruno Perel <brunoperel@gmail.com>
2016-12-17 16:47:30 +0100

    coa-updater: Add COA-box specific stuff

--- apps/coa-updater/Dockerfile
+++ apps/coa-updater/Dockerfile
@@ -1,2 +1,5 @@
 FROM bperel/mariadb-with-external-access:latest
 MAINTAINER Bruno Perel
+
+RUN apt-get update && \
+    apt-get install -y mariadb-client p7zip p7zip-full wget
---
+++ apps/coa-updater/scripts/coa-provision.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+. /home/container.properties
+
+inducks_path=/home/inducks
+isv_path=$inducks_path/isv
+
+rm -rf ${inducks_path}
+mkdir -p ${isv_path}
+
+cd ${inducks_path}
+
+wget http://coa.inducks.org/inducks/isv.7z && 7zr x isv.7z && rm isv.7z
+for f in ${isv_path}/*.isv; do iconv -f utf-8 -t utf-8 -c "$f" > "$f.clean" && mv -f "$f.clean" "$f"; done # Ignore lines with invalid UTF-8 characters
+mv ${isv_path}/createtables.sql ${inducks_path}
+
+cp ${inducks_path}/createtables.sql ${inducks_path}/createtables_clean.sql
+
+perl -0777 -i -pe 's%(CREATE TABLE (?:IF NOT EXISTS )?induckspriv[^;]+;)|([^\n]*induckspriv[^\n]*)%%gms' ${inducks_path}/createtables_clean.sql # Remove mentions of inducks_priv* tables
+perl -0777 -i -pe 's%(# End of file)$%ALTER TABLE inducks_entry ADD FULLTEXT INDEX entryTitleFullText(title);\n\n\1%gms' ${inducks_path}/createtables_clean.sql # Add full text index on entry titles
+
+mysql --user=root --password=${DB_PASSWORD} -e 'CREATE DATABASE IF NOT EXISTS `coa` /*!40100 DEFAULT CHARACTER SET utf8 */;'
+
+set +x
+echo "mysql -v --user=root --password=xxxxxxxx --default_character_set utf8 coa --local_infile=1 < ${inducks_path}/createtables_clean.sql" 1>&2
+mysql -v --user=root --password=${DB_PASSWORD} --default_character_set utf8 coa --local_infile=1 < ${inducks_path}/createtables_clean.sql
