a6aa797286d04d481277bf43d80a63fa1a8534c1
Bruno Perel <brunoperel@gmail.com>
2017-01-17 22:43:42 +0100

    Small fixes

--- apps/stats-updater/scripts/calculate-stats.sh
+++ apps/stats-updater/scripts/calculate-stats.sh
@@ -24,12 +24,12 @@ mysql -v --user=root --password=${DB_PASSWORD} < ${sh_dir}/sql/0_load-users-issu
 mysql -v --user=root --password=${COA_BOX_DB_PASSWORD} -h ${COA_BOX_HOST} < ${sh_dir}/sql/0_load-users-authors.sql
 
 # Get COA-related information
-mysql -v --user=root --password=${COA_BOX_DB_PASSWORD} -s -h ${COA_BOX_HOST} < ${sh_dir}/sql/1_coa-get-stories-publications.sql 2>&1 > ${coa_results_dir}/histoires_publications.sql
-mysql -v --user=root --password=${COA_BOX_DB_PASSWORD} -s -h ${COA_BOX_HOST} < ${sh_dir}/sql/2_coa-get-authors-stories.sql      2>&1 > ${coa_results_dir}/auteurs_histoires.sql
+mysql --user=root --password=${COA_BOX_DB_PASSWORD} -s -h ${COA_BOX_HOST} < ${sh_dir}/sql/1_coa-get-stories-publications.sql > ${coa_results_dir}/histoires_publications.csv 2>&1
+mysql --user=root --password=${COA_BOX_DB_PASSWORD} -s -h ${COA_BOX_HOST} < ${sh_dir}/sql/2_coa-get-authors-stories.sql      > ${coa_results_dir}/auteurs_histoires.csv 2>&1
 
 # Insert the data calculated from the COA database into the local dm_stats DB
-mysql -v --user=root --password=${DB_PASSWORD} dm_stats -e "LOAD DATA LOW_PRIORITY LOCAL INFILE '${coa_results_dir}/histoires_publications.sql' INTO TABLE dm_stats.histoires_publications; SHOW WARNINGS;"
-mysql -v --user=root --password=${DB_PASSWORD} dm_stats -e "LOAD DATA LOW_PRIORITY LOCAL INFILE '${coa_results_dir}/auteurs_histoires.sql' INTO TABLE dm_stats.auteurs_histoires; SHOW WARNINGS;"
+mysql -v --user=root --password=${DB_PASSWORD} dm_stats -e "LOAD DATA LOW_PRIORITY LOCAL INFILE '${coa_results_dir}/histoires_publications.csv' INTO TABLE dm_stats.histoires_publications; SHOW WARNINGS;"
+mysql -v --user=root --password=${DB_PASSWORD} dm_stats -e "LOAD DATA LOW_PRIORITY LOCAL INFILE '${coa_results_dir}/auteurs_histoires.csv' INTO TABLE dm_stats.auteurs_histoires; SHOW WARNINGS;"
 
 # Calculate the statistics locally
 mysql -v --user=root --password=${DB_PASSWORD} < ${sh_dir}/sql/3_calculate-stats.sql
