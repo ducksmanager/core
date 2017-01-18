5149dbd09d15447bd658137b4aad304b78dc70d1
Bruno Perel <brunope@qliro.local>
2017-01-18 23:10:00 +0000

    CREATE actual PKs instead of indexes

--- apps/stats-updater/scripts/sql/ddl/create-stats-tables.sql
+++ apps/stats-updater/scripts/sql/ddl/create-stats-tables.sql
@@ -1,11 +1,11 @@
 CREATE TABLE dm_stats.auteurs_histoires
 (
-  personcode VARCHAR(22) NOT NULL,
-  storycode VARCHAR(19) NOT NULL
+    personcode VARCHAR(22) NOT NULL,
+    storycode VARCHAR(19) NOT NULL,
+    CONSTRAINT `PRIMARY` PRIMARY KEY (personcode, storycode)
 );
 CREATE INDEX index_person ON dm_stats.auteurs_histoires (personcode);
 CREATE INDEX index_storycode ON dm_stats.auteurs_histoires (storycode);
-CREATE INDEX pk ON dm_stats.auteurs_histoires (personcode, storycode);
 
 CREATE TABLE dm_stats.histoires_publications
 (
@@ -19,22 +19,23 @@ CREATE INDEX index_story ON dm_stats.histoires_publications (storycode);
 
 CREATE TABLE dm_stats.utilisateurs_histoires_manquantes
 (
-  ID_User INT(11),
-  personcode VARCHAR(22),
-  storycode VARCHAR(19)
+    ID_User INT(11) NOT NULL,
+    personcode VARCHAR(22) NOT NULL,
+    storycode VARCHAR(19) NOT NULL,
+    CONSTRAINT `PRIMARY` PRIMARY KEY (ID_User, personcode, storycode)
 );
 
 CREATE TABLE dm_stats.utilisateurs_publications_manquantes
 (
-  ID_User INT(11) NOT NULL,
-  personcode VARCHAR(22) NOT NULL,
-  storycode VARCHAR(19) NOT NULL,
-  publicationcode VARCHAR(12) NOT NULL,
-  issuenumber VARCHAR(12) NOT NULL,
-  Notation TINYINT(3) unsigned NOT NULL
+    ID_User INT(11) NOT NULL,
+    personcode VARCHAR(22) NOT NULL,
+    storycode VARCHAR(19) NOT NULL,
+    publicationcode VARCHAR(12) NOT NULL,
+    issuenumber VARCHAR(12) NOT NULL,
+    Notation TINYINT(3) unsigned NOT NULL,
+    CONSTRAINT `PRIMARY` PRIMARY KEY (ID_User, storycode, publicationcode, issuenumber)
 );
 CREATE INDEX issue ON dm_stats.utilisateurs_publications_manquantes (ID_User, publicationcode, issuenumber);
-CREATE INDEX PK ON dm_stats.utilisateurs_publications_manquantes (ID_User, storycode, publicationcode, issuenumber);
 CREATE INDEX user_stories ON dm_stats.utilisateurs_publications_manquantes (ID_User, personcode, storycode);
 
 CREATE TABLE dm_stats.utilisateurs_publications_suggerees
