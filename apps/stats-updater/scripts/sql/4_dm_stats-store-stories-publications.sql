22495d7c43c32c2bf595c6d75e458a5eef693fcb
Bruno Perel <brunoperel@gmail.com>
2019-09-09 10:21:53 +0200

    Fix typo

--- apps/stats-updater/scripts/sql/4_dm_stats-store-stories-publications.sql
+++ apps/stats-updater/scripts/sql/4_dm_stats-store-stories-publications.sql
@@ -2,6 +2,6 @@ LOAD DATA LOW_PRIORITY
 LOCAL INFILE 'csv_results_dir/histoires_publications.csv'
 INTO TABLE histoires_publications
 (storycode, publicationcode, issuenumber, @oldestdate)
-SET @oldestdate = IFNULL(STR_TO_DATE(@oldestdate, '%Y-%m-%d'), STR_TO_DATE(@oldestdate, '%Y-%m'));
+SET oldestdate = IFNULL(STR_TO_DATE(@oldestdate, '%Y-%m-%d'), STR_TO_DATE(@oldestdate, '%Y-%m'));
 
 SHOW WARNINGS;
