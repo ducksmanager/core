94e66b915383c0ea1e22b57eff73b47207716fd3
Bruno Perel <brunoperel@gmail.com>
2020-06-10 22:46:00 +0200

    edgecreator: Extract enableDragResize

--- apps/edgecreator/components/renders/GradientRender.vue
+++ apps/edgecreator/components/renders/GradientRender.vue
@@ -32,8 +32,6 @@
 <script>
 import stepOptionsMixin from '@/mixins/stepOptionsMixin'
 
-const interact = require('interactjs')
-
 export default {
   mixins: [stepOptionsMixin],
 
@@ -53,21 +51,7 @@ export default {
 
   methods: {
     onOptionsSet() {
-      const vm = this
-      interact(this.$refs.rect)
-        .draggable({
-          onmove: (event) => {
-            vm.options.x += event.dx / vm.zoom
-            vm.options.y += event.dy / vm.zoom
-          }
-        })
-        .resizable({
-          edges: { right: true, bottom: true }
-        })
-        .on('resizemove', (event) => {
-          vm.options.width = event.rect.width / vm.zoom
-          vm.options.height = event.rect.height / vm.zoom
-        })
+      this.enableDragResize(this.$refs.rect)
     },
     async getOptionsFromDb() {
       return {
