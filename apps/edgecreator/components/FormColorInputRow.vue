1bafeb1d16eb3f5bac554a1e56cdafb2c4872962
Bruno Perel <brunoperel@gmail.com>
2020-06-10 20:39:03 +0200

    Extract form-color-input-row component

---
+++ apps/edgecreator/components/FormColorInputRow.vue
@@ -0,0 +1,56 @@
+<template>
+  <form-input-row
+    :option-name="optionName"
+    :label="`Color${optionName === 'Color' ? '' : ` (${optionName})`}`"
+    type="color"
+    :options="options"
+    :disabled="options[optionName] === 'transparent'"
+    ><input
+      v-if="canBeTransparent"
+      :id="`${optionName}-transparent`"
+      :checked="options[optionName] === 'transparent'"
+      type="checkbox"
+      @change="
+        $root.$emit(
+          'set-option',
+          optionName,
+          $event.currentTarget.checked ? 'transparent' : originalColor
+        )
+      "
+    />
+    <label v-if="canBeTransparent" :for="`${optionName}-transparent`"
+      >Transparent</label
+    >
+  </form-input-row>
+</template>
+<script>
+import FormInputRow from '~/components/FormInputRow'
+
+export default {
+  components: { FormInputRow },
+  props: {
+    options: {
+      type: Object,
+      required: true
+    },
+    optionName: {
+      type: String,
+      required: true
+    },
+    canBeTransparent: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    let originalColor = this.options[this.optionName]
+    if (originalColor === 'transparent') {
+      originalColor = '#000000'
+    }
+    return {
+      originalColor
+    }
+  }
+}
+</script>
+<style></style>
