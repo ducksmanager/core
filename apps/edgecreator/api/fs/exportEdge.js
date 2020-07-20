d251c16c043e874cbdcad021ffc090f83db2a35e
Bruno Perel <brunoperel@gmail.com>
2020-07-20 20:00:00 +0100

    edgecreator: Ensure that parent directories exist before exporting an edge

--- apps/edgecreator/api/fs/exportEdge.js
+++ apps/edgecreator/api/fs/exportEdge.js
@@ -2,11 +2,9 @@ const fs = require('fs')
 
 export default function (req, res) {
   const { country, magazine, issuenumber, content } = req.body
-  fs.writeFile(
-    `${process.env.EDGES_PATH}/${country}/gen/${magazine}.${issuenumber}.svg`,
-    content,
-    function () {
-      res.end()
-    }
-  )
+  const path = `${process.env.EDGES_PATH}/${country}/gen/${magazine}.${issuenumber}.svg`
+  fs.mkdirSync(require('path').dirname(path), { recursive: true })
+  fs.writeFile(path, content, function () {
+    res.end()
+  })
 }
