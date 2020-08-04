fe3d69c7f374d187d90f4c21582d702b35bf99cd
Bruno Perel <brunoperel@gmail.com>
2020-08-04 20:00:00 +0200

    Show WIP SVG edges on dashboard

---
+++ apps/edgecreator/mixins/svgUtilsMixin.js
@@ -0,0 +1,33 @@
+const DOMParser = require('xmldom').DOMParser
+
+export default {
+  methods: {
+    getSvgMetadata(svgChildNodes, metadataType) {
+      return svgChildNodes
+        .filter(
+          (node) =>
+            node.nodeName === 'metadata' &&
+            node.attributes.getNamedItem('type').nodeValue === metadataType
+        )
+        .map((metadataNode) => metadataNode.textContent.trim())
+    },
+    async loadSvgFromString(country, magazine, issuenumber, publishedVersion) {
+      const edgeUrl = this.getEdgeUrl(country, magazine, issuenumber, 'svg', publishedVersion)
+      const svgString = await this.$axios.$get(edgeUrl)
+      if (!svgString) {
+        return new Error(`No SVG found : ${edgeUrl}`)
+      }
+      const doc = new DOMParser().parseFromString(svgString, 'image/svg+xml')
+      const svgElement = doc.getElementsByTagName('svg')[0]
+      const svgChildNodes = Object.values(svgElement.childNodes)
+
+      return { doc, svgElement, svgChildNodes }
+    },
+    getEdgeUrl(country, magazine, issuenumber, extension, publishedVersion) {
+      return (
+        `${process.env.EDGES_URL}/${country}/gen/` +
+        `${publishedVersion ? '' : '_'}${magazine}.${issuenumber}.${extension}`
+      )
+    },
+  },
+}
