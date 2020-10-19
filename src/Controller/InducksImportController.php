4c73f6ca9581ec1b3f750f7a1868f197e52cee0f
Bruno Perel <brunoperel@gmail.com>
2020-10-19 21:45:00 +0000

    Use generic template

--- src/Controller/InducksImportController.php
+++ src/Controller/InducksImportController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class InducksImportController extends AbstractController
 {
@@ -27,9 +28,10 @@ class InducksImportController extends AbstractController
      *     path="/inducks/import"
      * )
      */
-    public function import(UserService $userService): Response
+    public function import(UserService $userService, TranslatorInterface $translator): Response
     {
-        return $this->render("collection.twig", [
+        return $this->render("bare.twig", [
+            'title' => $translator->trans('IMPORTER_INDUCKS'),
             'username' => $userService->getCurrentUsername(),
             'vueProps' => [
                 'component' => 'Site',
