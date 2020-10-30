fa59bc5f11a93ccd2a58fa4ba39c1574b823e706
Bruno Perel <brunoperel@gmail.com>
2020-10-30 18:36:15 +0100

    Init Book component

--- assets/js/store/modules/coa.js
+++ assets/js/store/modules/coa.js
@@ -1,23 +1,26 @@
+import Vue from 'vue'
 import axios from "axios";
 
 const URL_PREFIX_COUNTRIES = `/api/coa/list/countries/${window.locale}`
 const URL_PREFIX_PUBLICATIONS = '/api/coa/list/publications/'
 const URL_PREFIX_ISSUES = '/api/coa/list/issues/'
 const URL_PREFIX_AUTHORS = '/api/coa/authorsfullnames/'
+const URL_PREFIX_URLS = '/api/coa/list/urls/'
 const URL_ISSUE_COUNTS = '/api/coa/list/issues/count'
 
 export default {
-    namespaced: true,
-    state: () => ({
-        countryNames: null,
-        publicationNames: null,
-        publicationNamesFullCountries: [],
-        personNames: null,
-        issueNumbers: null,
-        issueDetails: null,
-        isLoadingCountryNames: false,
-        issueCounts: null,
-    }),
+  namespaced: true,
+  state: () => ({
+    countryNames: null,
+    publicationNames: null,
+    publicationNamesFullCountries: [],
+    personNames: null,
+    issueNumbers: null,
+    issueUrls: {},
+    issueDetails: null,
+    isLoadingCountryNames: false,
+    issueCounts: null,
+  }),
 
   mutations: {
     setCountryNames(state, countryNames) {
@@ -47,7 +50,7 @@ export default {
       state.issueNumbers = issueNumbers
     },
     setIssueUrls(state, {issueCode, issueUrls}) {
-      state.issueUrls[issueCode] = issueUrls
+      Vue.set(state.issueUrls, issueCode, issueUrls)
     },
     setIssueCounts(state, issueCounts) {
       state.issueCounts = issueCounts
@@ -105,42 +108,48 @@ export default {
         });
     },
 
-        fetchIssueNumbers: async ({ state, commit, dispatch }, publicationCodes) => {
-            const newPublicationCodes = [...new Set(publicationCodes.filter(publicationCode =>
-                !Object.keys(state.issueNumbers || {}).includes(publicationCode)
-            ))]
-            return newPublicationCodes.length && commit("setIssueNumbers", {
-                ...(state.issueNumbers || {}),
-                ...await dispatch('getChunkedRequests', {
-                    url: URL_PREFIX_ISSUES,
-                    parametersToChunk: newPublicationCodes,
-                    chunkSize: 1
-                }).then(data => data.reduce((acc, result) => ({
-                    ...acc,
-                    [result.config.url.replace(URL_PREFIX_ISSUES, '')]: result.data.map(issueNumber => issueNumber.replace(/ /g, ''))
-                }), {}))
-            });
-        },
+    fetchIssueNumbers: async ({state, commit, dispatch}, publicationCodes) => {
+      const newPublicationCodes = [...new Set(publicationCodes.filter(publicationCode =>
+        !Object.keys(state.issueNumbers || {}).includes(publicationCode)
+      ))]
+      return newPublicationCodes.length && commit("setIssueNumbers", {
+        ...(state.issueNumbers || {}),
+        ...await dispatch('getChunkedRequests', {
+          url: URL_PREFIX_ISSUES,
+          parametersToChunk: newPublicationCodes,
+          chunkSize: 1
+        }).then(data => data.reduce((acc, result) => ({
+          ...acc,
+          [result.config.url.replace(URL_PREFIX_ISSUES, '')]: result.data.map(issueNumber => issueNumber.replace(/ /g, ''))
+        }), {}))
+      });
+    },
 
-        fetchIssueCounts: async ({ state, commit }) => {
-            if (!state.issueCounts) {
-                const issueCounts = (await axios.get(URL_ISSUE_COUNTS)).data;
-                commit("setIssueCounts", issueCounts)
-            }
-        },
+    fetchIssueCounts: async ({state, commit}) => {
+      if (!state.issueCounts) {
+        const issueCounts = (await axios.get(URL_ISSUE_COUNTS)).data;
+        commit("setIssueCounts", issueCounts)
+      }
+    },
 
-        fetchIssueDetails: async ({ state, commit }, { publicationCode, issueNumber }) => {
+    fetchIssueUrls: async ({state, commit}, {publicationCode, issueNumber}) => {
+      const issueCode = `${publicationCode} ${issueNumber}`
+      if (!state.issueUrls[issueCode]) {
+        commit("setIssueUrls", {
+          issueCode,
+          issueUrls: (await axios.get(`${URL_PREFIX_URLS + publicationCode}/${issueNumber}`)).data
+        })
+      }
+    },
 
-        },
-
-        getChunkedRequests: async (_, {url, parametersToChunk, chunkSize}) =>
-            await Promise.all(
-                await Array.from({length: Math.ceil(parametersToChunk.length / chunkSize)}, (v, i) =>
-                    parametersToChunk.slice(i * chunkSize, i * chunkSize + chunkSize)
-                ).reduce(async (acc, codeChunk) =>
-                        (await acc).concat(await axios.get(`${url}${codeChunk.join(',')}`)),
-                    Promise.resolve([])
-                )
-            )
-    }
+    getChunkedRequests: async (_, {url, parametersToChunk, chunkSize}) =>
+      await Promise.all(
+        await Array.from({length: Math.ceil(parametersToChunk.length / chunkSize)}, (v, i) =>
+          parametersToChunk.slice(i * chunkSize, i * chunkSize + chunkSize)
+        ).reduce(async (acc, codeChunk) =>
+            (await acc).concat(await axios.get(`${url}${codeChunk.join(',')}`)),
+          Promise.resolve([])
+        )
+      )
+  }
 }
