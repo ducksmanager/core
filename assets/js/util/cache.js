af053c06b3b7fca470733b427bf710674bcd8d58
Bruno Perel <brunoperel@gmail.com>
2020-12-07 22:46:10 +0100

    Fix COA cache expiration setup

--- assets/js/util/cache.js
+++ assets/js/util/cache.js
@@ -8,8 +8,13 @@ inAnHour.setHours(inAnHour.getHours() + 1)
 let inAMonth = new Date()
 inAMonth.setMonth(inAMonth.getMonth() + 1)
 
-const isCoaUpdatePassedToday = now.getHours() > 4
-const coaCacheExpiration = new Date(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate() + (isCoaUpdatePassedToday ? 1 : 0)}T04:00:00`)
+let coaCacheExpiration = new Date()
+if (now.getHours() >= 4) {
+  coaCacheExpiration.setDate(now.getDate() + 1)
+}
+coaCacheExpiration.setHours(4)
+coaCacheExpiration.setMinutes(0)
+coaCacheExpiration.setSeconds(0)
 
 const appCache = setupCache({
   maxAge: inAMonth,
@@ -27,6 +32,17 @@ const userCountCache = setupCache({
 const coaCache = setupCache({
   maxAge: coaCacheExpiration - now,
   store: localforage
+})
+
+// Remove infinite cache set by mistake
+localforage.keys((error, keys) => {
+  keys.forEach(key => {
+    localforage.getItem(key, (error, value) => {
+      if (isNaN(value) || !value) {
+        localforage.removeItem(key)
+      }
+    })
+  })
 })
 
 export {
