dc09e07a32255bbc0bfa8b803f601be21fa78431
Bruno Perel <brunoperel@gmail.com>
2020-12-10 20:05:00 +0000

    edgecreator: Invert role check

--- apps/edgecreator/middleware/is-editor.js
+++ apps/edgecreator/middleware/is-editor.js
@@ -1,5 +1,5 @@
 export default function ({ $gates, redirect }) {
-  if ($gates.unlessRole('display')) {
+  if (!$gates.unlessRole('display')) {
     return redirect({ path: '/', hash: '#403' })
   }
 }
