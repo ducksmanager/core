183943125648a0f36f75ee7d19a1c82ac6c8d9e0
Bruno Perel <brunoperel@gmail.com>
2020-12-15 20:15:00 +0000

    Export plugins Allow to override backend data config

--- assets/js/plugins/backendDataPlugin.js
+++ assets/js/plugins/backendDataPlugin.js
@@ -1,10 +1,13 @@
 export default {
-  install: function (Vue) {
+  install: function (Vue, options) {
     Vue.mixin({
-      computed: ['locale', 'l10nUrl', 'imagePath', 'commit', 'userId', 'username'].reduce((acc, key) => ({
-        ...acc,
-        [key]: () => window[key] || undefined
-      }), {})
+      computed: ['locale', 'l10nUrl', 'imagePath', 'commit', 'userId', 'username'].reduce((acc, key) => {
+        const value = (options.overrides && options.overrides[key]) || window[key] || undefined
+        return value !== undefined ? {
+          ...acc,
+          [key]: () => value
+        } : acc;
+      }, {})
     })
   }
 }
