217a5370a9521891fba7ec1bcc035c263c36f32e
Bruno Perel <brunoperel@gmail.com>
2020-12-16 20:30:00 +0000

    Put edge badges on a separate line

--- apps/edgecreator/components/EdgeLink.vue
+++ apps/edgecreator/components/EdgeLink.vue
@@ -5,13 +5,16 @@
       :publicationname="publicationNames[publicationcode]"
       :issuenumber="issuenumber"
       hide-condition
-    /><b-badge v-if="v3">v3</b-badge>
-    <b-badge v-for="designer in designers" :key="`designer-${designer}`"
-      >{{ $t('Designer') }}:{{ designer }}</b-badge
-    >
-    <b-badge v-for="photographer in photographers" :key="`photographer-${photographer}`"
-      >{{ $t('Photographer') }}:{{ photographer }}</b-badge
-    >
+    />
+    <div>
+      <b-badge v-if="v3">v3</b-badge>
+      <b-badge v-for="designer in designers" :key="`designer-${designer}`"
+        >{{ $t('Designer') }}:{{ designer }}</b-badge
+      >
+      <b-badge v-for="photographer in photographers" :key="`photographer-${photographer}`"
+        >{{ $t('Photographer') }}:{{ photographer }}</b-badge
+      >
+    </div>
   </span>
 </template>
 
