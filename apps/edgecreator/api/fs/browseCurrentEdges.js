5d75f29762444d52ee4932db38846b9f4332ae38
Bruno Perel <brunoperel@gmail.com>
2020-12-16 20:05:00 +0000

    edgecreator: Ignore accentuated file names when browsing through edges

--- apps/edgecreator/api/fs/browseCurrentEdges.js
+++ apps/edgecreator/api/fs/browseCurrentEdges.js
@@ -2,26 +2,29 @@ const fs = require('fs')
 const path = require('path')
 
 const edgePath = process.env.EDGES_PATH
+const REGEX_IS_BROWSABLE_FILE = /^[-+(). _A-Za-z\d]+$/
+const REGEX_IS_SVG_FILE = /\/_[^/]+\.svg$/
 
 export default function (req, res) {
-  const svgs = findInDir(edgePath, /\/_[^/]+\.svg$/)
+  const svgFiles = findInDir(edgePath, REGEX_IS_SVG_FILE)
   res.writeHeader(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify(svgs))
+  res.end(JSON.stringify(svgFiles))
 }
 
 const findInDir = (dir, filter, fileList = []) => {
   const files = fs.readdirSync(dir)
+  files
+    .filter((file) => REGEX_IS_BROWSABLE_FILE.test(file))
+    .forEach((file) => {
+      const filePath = path.join(dir, file)
+      const fileStat = fs.lstatSync(filePath)
 
-  files.forEach((file) => {
-    const filePath = path.join(dir, file)
-    const fileStat = fs.lstatSync(filePath)
-
-    if (fileStat.isDirectory()) {
-      findInDir(filePath, filter, fileList)
-    } else if (filter.test(filePath)) {
-      fileList.push(filePath.replace(edgePath, ''))
-    }
-  })
+      if (fileStat.isDirectory()) {
+        findInDir(filePath, filter, fileList)
+      } else if (filter.test(filePath)) {
+        fileList.push(filePath.replace(edgePath, ''))
+      }
+    })
 
   return fileList
 }
