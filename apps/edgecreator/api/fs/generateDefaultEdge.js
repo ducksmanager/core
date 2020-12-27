55a5cb82f4126199c468479c3a124a2d5776e3c8
Bruno Perel <brunoperel@gmail.com>
2020-12-27 17:42:56 +0100

    edgecreator: Add CORS headers for the /generate endpoint

--- apps/edgecreator/api/fs/generateDefaultEdge.js
+++ apps/edgecreator/api/fs/generateDefaultEdge.js
@@ -4,6 +4,16 @@ const svg2img = require('svg2img')
 const REGEX_EDGE_URL = /^edges\/([^/]+\/)gen\/_?([^.]+)\.([^.]+)\.(svg|png)?$/
 
 export default function (req, res) {
+  res.writeHeader(200, {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Access-Control-Allow-Headers':
+      'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,x-dm-user,x-dm-pass',
+  })
+  if (req.method === 'OPTIONS') {
+    res.end()
+  }
   const text = req.url.replace(/^\//, '').replace(REGEX_EDGE_URL, `$1$2 $3`)
 
   const content = fs
