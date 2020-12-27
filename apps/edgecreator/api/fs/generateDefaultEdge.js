7728f1a0dbdae37159afbe3ee09a2c80013c6ce9
Bruno Perel <brunoperel@gmail.com>
2020-12-27 15:37:57 +0100

    edgecreator: Support generating a default edge with the edge original URL as input

--- apps/edgecreator/api/fs/generateDefaultEdge.js
+++ apps/edgecreator/api/fs/generateDefaultEdge.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 const svg2img = require('svg2img')
 
+const REGEX_EDGE_URL = /^edges\/([^/]+\/)gen\/_?([^.]+)\.([^.]+)\.(svg|png)?$/
+
 export default function (req, res) {
+  const text = req.url.replace(/^\//, '').replace(REGEX_EDGE_URL, `$1$2 $3`)
+
   const content = fs
     .readFileSync('assets/default.svg')
     .toString()
-    .replace('My text', decodeURIComponent(req.url.replace(/^\//, '')))
+    .replace('My text', decodeURIComponent(text))
   svg2img(content, (error, buffer) => {
     if (error) {
       res.writeHeader(500)
