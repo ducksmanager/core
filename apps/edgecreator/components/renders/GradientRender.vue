55cae0243c7e9cb396538aa867e29d5b6214616a
Bruno Perel <brunoperel@gmail.com>
2021-01-06 15:24:34 +0100

    edgecreator: Fix gradients identical between edges even if they have different options, when editing several edges at the same time

--- apps/edgecreator/components/renders/GradientRender.vue
+++ apps/edgecreator/components/renders/GradientRender.vue
@@ -2,7 +2,7 @@
   <g>
     <defs>
       <linearGradient
-        id="gradient"
+        :id="gradientId"
         x1="0%"
         y1="0%"
         :x2="options.direction === 'Vertical' ? '0%' : '100%'"
@@ -12,7 +12,7 @@
         <stop offset="100%" :style="{ 'stop-color': options.colorEnd, 'stop-opacity': 1 }" />
       </linearGradient>
     </defs>
-    <rect ref="rect" v-bind="attributes" fill="url(#gradient)">
+    <rect ref="rect" v-bind="attributes" :fill="`url(#${gradientId})`">
       <metadata>{{ options }}</metadata>
     </rect>
   </g>
@@ -42,6 +42,12 @@ export default {
   data: () => ({
     attributeKeys: ['x', 'y', 'width', 'height'],
   }),
+
+  computed: {
+    gradientId() {
+      return Object.values(this.options).join('-')
+    },
+  },
 
   mounted() {
     this.enableDragResize(this.$refs.rect)
