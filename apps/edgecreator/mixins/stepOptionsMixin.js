3f20e8460013eef3526ba62a35ed18915ce25033
Bruno Perel <brunoperel@gmail.com>
2021-03-14 15:36:55 +0100

    edgecreator: Don't animate the hovered step during drag or resize

--- apps/edgecreator/mixins/stepOptionsMixin.js
+++ apps/edgecreator/mixins/stepOptionsMixin.js
@@ -69,33 +69,46 @@ export default {
     isColorOption(optionName) {
       return optionName.toLowerCase().includes('color') || ['fill', 'stroke'].includes(optionName)
     },
-    enableDragResize(image, { onmove = null, onresizemove = null } = {}) {
+    enableDragResize(element, { onmove = null, onresizemove = null } = {}) {
       const vm = this
-      interact(image)
+      interact(element)
         .draggable({
-          onmove:
-            onmove ||
-            (({ dx, dy }) => {
+          onmove: (e) => {
+            document.body.classList.add('interacting')
+            if (onmove) {
+              onmove(e)
+            } else {
+              const { dx, dy } = e
               vm.$root.$emit('set-options', {
                 x: vm.options.x + dx / vm.zoom / 3,
                 y: vm.options.y + dy / vm.zoom / 3,
               })
-            }),
+            }
+          },
+          onend: () => {
+            document.body.classList.remove('interacting')
+          },
         })
         .resizable({
           edges: { right: true, bottom: true },
         })
-        .on(
-          'resizemove',
-          onresizemove ||
-            (({ rect }) => {
-              const { width, height } = rect
-              vm.$root.$emit('set-options', {
-                width: width / vm.zoom,
-                height: height / vm.zoom,
-              })
+        .on('resizemove', (e) => {
+          document.body.classList.add('interacting')
+          if (onresizemove) {
+            onresizemove(e)
+          } else {
+            const { rect } = e
+            const { width, height } = rect
+            vm.$root.$emit('set-options', {
+              width: width / vm.zoom,
+              height: height / vm.zoom,
             })
-        )
+          }
+        })
+
+        .on('resizeend', () => {
+          document.body.classList.remove('interacting')
+        })
     },
   },
   mounted() {
