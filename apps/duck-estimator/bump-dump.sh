8ccfc57fb3003b909b249349b3365898e0746b0d
Bruno Perel <brunoperel@gmail.com>
2021-05-09 14:13:18 +0200

    duck-estimator: Fix envvar access

--- apps/duck-estimator/bump-dump.sh
+++ apps/duck-estimator/bump-dump.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 rm -rf duck-estimator-db
-git clone https://GITHUB_TOKEN@github.com/bperel/duck-estimator-db
+git clone https://"$GITHUB_TOKEN"@github.com/bperel/duck-estimator-db
 cd duck-estimator-db
 mv ../dump.csv .
 
-git config user.name GITHUB_NAME
-git config user.email GITHUB_EMAIL
+git config user.name "$GITHUB_NAME"
+git config user.email "$GITHUB_EMAIL"
 git add .
 git commit -m "Bump dump"
 git push origin master
