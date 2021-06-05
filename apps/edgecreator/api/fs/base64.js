cae9559f9b9a1187979ed5175815ec4aaf59b5d9
Bruno Perel <brunoperel@gmail.com>
2021-06-05 12:02:24 +0200

    edgecreator: Prevent crash when calculating the base64 of an unreadable image

--- apps/edgecreator/api/fs/base64.js
+++ apps/edgecreator/api/fs/base64.js
@@ -9,11 +9,19 @@ export default function (req, res) {
       : `${process.env.EDGES_URL}/${targetUrl}`
   request.get(url, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      const buffer = Buffer.from(body)
-      const dimensions = sizeOf(buffer)
-      const base64 = `data:${response.headers['content-type']};base64,${buffer.toString('base64')}`
-      res.writeHeader(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ dimensions, base64, url }))
+      try {
+        const buffer = Buffer.from(body)
+        const dimensions = sizeOf(buffer)
+        const base64 = `data:${response.headers['content-type']};base64,${buffer.toString(
+          'base64'
+        )}`
+        res.writeHeader(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ dimensions, base64, url }))
+      } catch (e) {
+        console.error(targetUrl + ' : ' + e)
+        res.statusCode = 404
+        res.end()
+      }
     } else {
       res.statusCode = response.statusCode
       res.end()
