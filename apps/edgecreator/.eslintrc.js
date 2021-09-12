2628c0318aa918dc994a5f11fd66a65edfd21936
Bruno Perel <brunoperel@gmail.com>
2021-09-12 11:02:42 +0200

    edgecreator: Run --fix in eslint script

--- apps/edgecreator/.eslintrc.js
+++ apps/edgecreator/.eslintrc.js
@@ -7,11 +7,7 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
+  extends: ['@nuxtjs', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
   plugins: ['prettier'],
   // add your custom rules here
   rules: {
