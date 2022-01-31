8035cc1f4404ba9658cc1a3ce103bfb816a4354e
Bruno Perel <brunoperel@gmail.com>
2022-01-31 22:01:55 +0100

    Put the base64 text image encoding under the browser's responsibility to allow caching

--- apps/edgecreator/components/renders/ImageRender.vue
+++ apps/edgecreator/components/renders/ImageRender.vue
@@ -16,9 +16,10 @@
 import { mapState } from 'vuex'
 
 import stepOptionsMixin from '@/mixins/stepOptionsMixin'
+import base64Mixin from '@/mixins/base64Mixin'
 
 export default {
-  mixins: [stepOptionsMixin],
+  mixins: [stepOptionsMixin, base64Mixin],
 
   props: {
     options: {
@@ -34,7 +35,6 @@ export default {
   },
 
   data: () => ({
-    image: { base64: null, width: null, height: null },
     attributeKeys: ['x', 'y', 'width', 'height'],
   }),
 
@@ -50,43 +50,15 @@ export default {
       immediate: true,
       async handler() {
         if (this.effectiveSource) {
-          try {
-            this.loadImage(
-              `${process.env.EDGES_URL_PUBLIC}/${this.country}/elements/${this.effectiveSource}`
-            )
-          } catch (e) {
-            console.error(
-              `Image could not be retrieved : ${this.effectiveSource}`
-            )
-            this.image = { base64: null, width: null, height: null }
-          }
+          this.loadImage(
+            `${process.env.EDGES_URL_PUBLIC}/${this.country}/elements/${this.effectiveSource}`
+          )
         }
       },
     },
   },
   async mounted() {
     this.enableDragResize(this.$refs.image)
-  },
-  methods: {
-    loadImage(src) {
-      const vm = this
-      const img = new Image()
-      img.crossOrigin = 'Anonymous'
-      img.onload = function () {
-        const canvas = document.createElement('CANVAS')
-        const ctx = canvas.getContext('2d')
-        canvas.height = this.naturalHeight
-        canvas.width = this.naturalWidth
-        ctx.drawImage(this, 0, 0)
-        vm.image = {
-          base64: canvas.toDataURL('png'),
-          width: this.naturalWidth,
-          height: this.naturalHeight,
-        }
-        vm.enableDragResize(vm.$refs.image)
-      }
-      img.src = src
-    },
   },
 }
 </script>
