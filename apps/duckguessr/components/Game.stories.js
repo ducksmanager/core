50cdc5ce28bc84c49eb5f9b38179221c90813283
Bruno Perel <brunoperel@gmail.com>
2022-04-25 21:41:10 +0200

    Fix Game stories

--- apps/duckguessr/components/Game.stories.js
+++ apps/duckguessr/components/Game.stories.js
@@ -15,17 +15,21 @@ export const Default = Template.bind({})
 Default.args = {
   availableTime: 15,
   chosenAuthor: 'DR',
-  currentRoundNumber: 2,
-  currentRoundScores: [
-    {
-      id: 100,
-      player_id: 1,
-      round_id: 10,
-      score_type_name: 'Correct author',
-      score: 100,
-      speed_bonus: 10,
-    },
-  ],
+  currentRound: {
+    round_number: 2,
+    round_scores: [
+      {
+        id: 100,
+        player_id: 1,
+        round_id: 10,
+        score_type_name: 'Correct author',
+        score: 100,
+        speed_bonus: 10,
+      },
+    ],
+    sitecode_url:
+      'https://res.cloudinary.com/dl7hskxab/image/upload/v1623338718/inducks-covers/thumbnails3/webusers/2008/09/us_zz1966b23x_001.jpg',
+  },
   authors: [
     { personcode: 'CB', personnationality: 'us', personfullname: 'Carl Barks' },
     { personcode: 'DR', personnationality: 'us', personfullname: 'Don Rosa' },
@@ -46,5 +50,4 @@ Default.args = {
   ],
   previousPersoncodes: ['CB', 'DR'],
   remainingTime: 5,
-  url: 'https://res.cloudinary.com/dl7hskxab/image/upload/v1623338718/inducks-covers/thumbnails3/webusers/2008/09/us_zz1966b23x_001.jpg',
 }
