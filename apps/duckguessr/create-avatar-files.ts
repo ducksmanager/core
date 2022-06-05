df8c82fdb12c453ab04b2126c6da80b4e24dd1de
Bruno Perel <brunoperel@gmail.com>
2022-06-05 17:06:54 +0200

    Convert create-avatar-files.js to Typescript

---
+++ apps/duckguessr/create-avatar-files.ts
@@ -0,0 +1,36 @@
+const { renameSync } = require('fs')
+const sharp = require('sharp')
+const { avatars, avatarDiskDiameter } = require('./types/avatars')
+
+const tree = sharp('static/Donald_Duck_Family_Tree.webp')
+
+for (const { character, position } of avatars) {
+  tree
+    .extract({
+      width: avatarDiskDiameter,
+      height: avatarDiskDiameter,
+      left: position[0] - avatarDiskDiameter / 2,
+      top: position[1] - avatarDiskDiameter / 2,
+    })
+    .toFile(`static/avatars/${character}.png`)
+    .then(() => {
+      console.log(`Image cropped and saved`)
+      const r = avatarDiskDiameter / 2
+      const circleShape = Buffer.from(`<svg><circle cx="${r}" cy="${r}" r="${r}" /></svg>`)
+
+      sharp(`static/avatars/${character}.png`)
+        .composite([
+          {
+            input: circleShape,
+            blend: 'dest-in',
+          },
+        ])
+        .toFile(`static/avatars/${character}_2.png`)
+        .then(() => {
+          renameSync(`static/avatars/${character}_2.png`, `static/avatars/${character}.png`)
+        })
+    })
+    .catch((err: any) => {
+      console.log(`An error occurred: ${err}`)
+    })
+}
---
+++ apps/duckguessr/types/avatars.ts
@@ -0,0 +1,31 @@
+export class Avatar {
+  character: string
+  restriction: {
+    roundsWon: number
+  } | null
+
+  position: [number, number]
+
+  constructor(character: string, restriction: { roundsWon: number }, position: [number, number]) {
+    this.character = character
+    this.restriction = restriction
+    this.position = position
+  }
+}
+
+export const avatars: Avatar[] = [
+  { character: 'Huey', restriction: null, position: [206, 89] },
+  { character: 'Dewey', restriction: null, position: [331, 89] },
+  { character: 'Louie', restriction: null, position: [457, 90] },
+  { character: 'Della Duck', restriction: null, position: [396, 232] },
+  { character: 'DD', restriction: null, position: [514, 235] },
+  { character: 'GL', restriction: null, position: [667, 234] },
+  { character: 'FE', restriction: null, position: [818, 236] },
+  { character: 'Whitewater Duck', restriction: null, position: [961, 239] },
+  { character: 'GU', restriction: null, position: [1143, 238] },
+  { character: 'Matilda McDuck', restriction: { roundsWon: 50 }, position: [92, 372] },
+  { character: 'US', restriction: { roundsWon: 100 }, position: [207, 371] },
+  { character: 'Hortense McDuck', restriction: { roundsWon: 50 }, position: [317, 370] },
+]
+
+export const avatarDiskDiameter = 102
