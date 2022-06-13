3a3480c57e6e85c86825bd7cc010517707758686
Bruno Perel <bruno.perel@firstvet.com>
2022-06-13 11:34:02 +0200

    Integrate Bootstrap-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ import eslintPlugin from "vite-plugin-eslint";
 
 export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@pinia/nuxt"],
-  css: ["@/assets/css/app.scss"],
+  css: [
+    "@/assets/css/app.scss",
+    "bootstrap/dist/css/bootstrap.css",
+    "bootstrap-vue-3/dist/bootstrap-vue-3.css",
+  ],
   intlify: {
     localeDir: "translations",
     vueI18n: "i18n.mjs",
---
+++ plugins/bootstrap.client.ts
@@ -0,0 +1,6 @@
+// plugins/bootstrap.client.ts
+import BootstrapVue3 from "bootstrap-vue-3";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(BootstrapVue3);
+});
