55936a299abde2edcb91b6e4df6e307d9ff48de3
Bruno Perel <brunoperel@gmail.com>
2022-06-14 23:07:59 +0200

    Hopefully fix joining a match as an anonymous user

--- apps/duckguessr/pages/matchmaking/_id.vue
+++ apps/duckguessr/pages/matchmaking/_id.vue
@@ -14,8 +14,8 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, ref, useRoute, useRouter } from '@nuxtjs/composition-api'
-import { io } from 'socket.io-client'
+import { computed, ref, useRoute, useRouter, watch } from '@nuxtjs/composition-api'
+import { io, Socket } from 'socket.io-client'
 import { useCookies } from '@vueuse/integrations/useCookies'
 import { MatchDetails } from '~/types/matchDetails'
 import { userStore } from '~/store/user'
@@ -29,11 +29,7 @@ const gameId = parseInt(route.value.params.id)
 
 const isAnonymous = computed(() => userStore().isAnonymous)
 
-const matchmakingSocket = io(`${process.env.SOCKET_URL}/matchmaking/${gameId}`, {
-  auth: {
-    cookie: useCookies().getAll(),
-  },
-})
+const matchmakingSocket = ref(null as Socket | null)
 
 const addPlayer = (username: string) => {
   if (!playersUsernames.value.includes(username)) {
@@ -46,43 +42,51 @@ const removePlayer = (username: string) => {
 }
 
 const startMatch = () => {
-  matchmakingSocket.emit('startMatch')
+  matchmakingSocket.value!.emit('startMatch')
 }
 
 const addBot = () => {
-  matchmakingSocket.emit('addBot')
+  matchmakingSocket.value!.emit('addBot')
 }
 
 const removeBot = () => {
-  matchmakingSocket.emit('removeBot')
+  matchmakingSocket.value!.emit('removeBot')
 }
 
-onMounted(() => {
-  matchmakingSocket
-    .on('playerJoined', (username: string) => {
-      console.debug(`${username} is also ready`)
-      addPlayer(username)
-    })
-    .on('playerLeft', (username: string) => {
-      console.debug(`${username} has left`)
-      removePlayer(username)
-    })
-    .on('matchStarts', () => {
-      setTimeout(() => {
-        // Leave time for the joinMatch callback to be called
-        console.debug(`Match starts on game ${gameId}`)
-        matchmakingSocket.close()
-        router.replace(`/game/${gameId}`)
-      }, 200)
-    })
-
-  setTimeout(() => {
-    matchmakingSocket.emit('joinMatch', ({ players, isBotAvailable }: MatchDetails) => {
-      isBotAvailableForGame.value = isBotAvailable
-      for (const player of players) {
-        addPlayer(player.username)
-      }
-    })
-  }, 200)
-})
+watch(
+  () => userStore().user,
+  (value) => {
+    if (value) {
+      console.log(useCookies().getAll())
+      matchmakingSocket.value = io(`${process.env.SOCKET_URL}/matchmaking/${gameId}`, {
+        auth: {
+          cookie: useCookies().getAll(),
+        },
+      })
+        .on('playerJoined', (username: string) => {
+          console.debug(`${username} is also ready`)
+          addPlayer(username)
+        })
+        .on('playerLeft', (username: string) => {
+          console.debug(`${username} has left`)
+          removePlayer(username)
+        })
+        .on('matchStarts', () => {
+          setTimeout(() => {
+            // Leave time for the joinMatch callback to be called
+            console.debug(`Match starts on game ${gameId}`)
+            matchmakingSocket.value!.close()
+            router.replace(`/game/${gameId}`)
+          }, 200)
+        })
+        .emit('joinMatch', ({ players, isBotAvailable }: MatchDetails) => {
+          isBotAvailableForGame.value = isBotAvailable
+          for (const player of players) {
+            addPlayer(player.username)
+          }
+        })
+    }
+  },
+  { immediate: true }
+)
 </script>
