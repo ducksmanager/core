178b631b286504f037db1130b56e184467af3536
Bruno Perel <brunoperel@gmail.com>
2022-06-21 19:06:16 +0200

    Add back "You are not connected" banner

--- apps/duckguessr/pages/index.vue
+++ apps/duckguessr/pages/index.vue
@@ -1,26 +1,29 @@
 <template>
-  <b-card-group deck>
-    <b-card
-      v-for="{ title, description, name, images, authors } in datasets"
-      :key="name"
-      :title="t(title)"
-      img-top
-      align="center"
-      :style="matchCreationSocket ? '' : 'pointer-events: none'"
-      @click="createMatch(name)"
-    >
-      <b-card-header class="my-2 bg-transparent border-0 small">
-        {{ t(description) }}
-      </b-card-header>
-      <b-card-footer>
-        {{ t('Images') }}: {{ images }}, {{ t('authors') }}: {{ authors }}
-      </b-card-footer>
-    </b-card>
-  </b-card-group>
+  <div>
+    <alert-not-connected v-if="isAnonymous === true" />
+    <b-card-group deck>
+      <b-card
+        v-for="{ title, description, name, images, authors } in datasets"
+        :key="name"
+        :title="t(title)"
+        img-top
+        align="center"
+        :style="matchCreationSocket ? '' : 'pointer-events: none'"
+        @click="createMatch(name)"
+      >
+        <b-card-header class="my-2 bg-transparent border-0 small">
+          {{ t(description) }}
+        </b-card-header>
+        <b-card-footer>
+          {{ t('Images') }}: {{ images }}, {{ t('authors') }}: {{ authors }}
+        </b-card-footer>
+      </b-card>
+    </b-card-group>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref, useRouter, watch } from '@nuxtjs/composition-api'
+import { computed, onMounted, ref, useRouter, watch } from '@nuxtjs/composition-api'
 import { useI18n } from 'nuxt-i18n-composable'
 import { useAxios } from '@vueuse/integrations/useAxios'
 import { io, Socket } from 'socket.io-client'
@@ -32,6 +35,7 @@ const { t } = useI18n()
 
 const datasets = ref([] as Array<any>)
 
+const isAnonymous = computed(() => userStore().isAnonymous)
 const matchCreationSocket = ref(null as Socket | null)
 
 const createMatch = (datasetName: string) => {
