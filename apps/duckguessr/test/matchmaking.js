9def622fd948468a56760c0fb90a3ece9584e790
Bruno Perel <brunoperel@gmail.com>
2022-06-22 21:37:12 +0200

    Make players guess in test

--- apps/duckguessr/test/matchmaking.js
+++ apps/duckguessr/test/matchmaking.js
@@ -12,13 +12,25 @@ function joinMatch(I, page) {
   I.seeElement('.card.player')
 }
 
-Scenario('Create match between multiple users and start game', async ({ I }) => {
-  const url = await createMatch(I)
-  for (let i = 0; i < 2; i++) {
-    await session(`user ${i}`, () => {
-      joinMatch(I, url)
-    })
-  }
-  I.click('.btn-success')
-  I.amOnPage(url.replace('matchmaking', 'game'))
+Scenario('Create match between multiple users and start game', ({ I }) => {
+  const possibleAuthors = 9
+  const extraPlayers = 2
+  session('match-creator', async () => {
+    const url = await createMatch(I)
+    for (let i = 0; i < extraPlayers; i++) {
+      session(`user ${i}`, () => {
+        joinMatch(I, url)
+      })
+    }
+    I.click('.btn-success')
+    I.amOnPage(url.replace('matchmaking', 'game'))
+    I.waitForElement('#author-list .author.selectable', 15)
+    for (let i = 0; i < extraPlayers; i++) {
+      await session(`user ${i}`, () => {
+        I.click(
+          `#author-list .author:nth-child(${Math.floor(Math.random() * possibleAuthors + 1)})`
+        )
+      })
+    }
+  })
 })
