fdaee08c49d424253cd77d9bc6700c9c61aeeefa
Bruno Perel <brunoperel@gmail.com>
2022-07-23 23:07:02 +0200

    Rearrange menu items

--- apps/whattheduck/src/components/Menu.vue
+++ apps/whattheduck/src/components/Menu.vue
@@ -1,6 +1,15 @@
 <template>
   <ion-menu menu-id="app-menu" content-id="main-content">
+    <ion-header>
+      <ion-toolbar color="light">
+        <ion-title>What The Duck</ion-title>
+      </ion-toolbar>
+    </ion-header>
     <ion-content>
+      <ion-grid>
+        <ion-row>
+      Medals
+        </ion-row>
       <ion-list>
         <ion-item button @click="menuNavigation('/collection')">
           <ion-label>My collection</ion-label>
@@ -9,12 +18,21 @@
           <ion-label>About</ion-label>
         </ion-item>
       </ion-list>
+        <ion-row className="ion-align-items-end" style="flex-direction: column">
+          <ion-col className="ion-align-self-center">
+            Report a problem
+          </ion-col>
+          <ion-col>
+            Logout
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-menu>
 </template>
 <script setup lang="ts">
 import {
-  IonMenu, IonContent, IonItem, IonList, IonLabel, useIonRouter, menuController,
+  IonHeader, IonMenu, IonContent, IonGrid, IonCol, IonRow, IonItem, IonList, IonLabel, IonToolbar, IonTitle, useIonRouter, menuController,
 } from "@ionic/vue";
 
 const router = useIonRouter();
@@ -23,3 +41,15 @@ const menuNavigation = (url: string) => {
   router.push(url);
 }
 </script>
+
+<style lang="scss">
+ion-grid {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  ion-row {
+    flex-grow: 1;
+  }
+}
+</style>
