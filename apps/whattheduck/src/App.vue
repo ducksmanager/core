b9e86576583676887024fbd4669f05efdae8d068
Bruno Perel <brunoperel@gmail.com>
2022-07-25 23:25:20 +0200

    Always use dark mode Rename Menu to Drawer

--- apps/whattheduck/src/App.vue
+++ apps/whattheduck/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <ion-app>
-    <Menu></Menu>
+    <Drawer />
     <ion-router-outlet id="main-content"/>
   </ion-app>
 </template>
@@ -8,14 +8,14 @@
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import Menu from './components/Menu.vue';
+import Drawer from './components/Drawer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet,
-    Menu
+    Drawer
   }
 });
 </script>
