46211a49f21a5e5c65652536591151d4ea79e0d6
Bruno Perel <bruno.perel@firstvet.com>
2022-07-28 17:12:25 +0200

    Fiw UserPopover again

--- assets/js/components/UserPopover.vue
+++ assets/js/components/UserPopover.vue
@@ -1,67 +1,71 @@
 <template>
-  <span :id="id" class="username fw-bold" style="cursor: help">{{
-    stats.username
-  }}</span>
-  <b-popover
-    :target="id"
+  <Popper
     placement="top"
-    boundary="viewport"
-    triggers="hover focus"
-    :delay="0"
+    arrow
+    manual
+    teleport="body"
+    :show="isOverPopup || isOverPopupText"
   >
-    <b-card>
-      <b-card-header>
-        <h4>{{ stats.username }}</h4>
-        <div v-if="stats.presentationSentence">
-          {{ stats.presentationSentence }}
+    <span
+      class="username fw-bold"
+      @mouseenter="isOverPopupText = true"
+      @mouseleave="closePopupSoon"
+      >{{ stats.username }}</span
+    >
+    <template #content>
+      <b-card
+        body-class="p-0"
+        @mouseenter="isOverPopup = true"
+        @mouseleave="isOverPopup = false"
+      >
+        <b-card-header>
+          <h4>{{ stats.username }}</h4>
+          <div v-if="stats.presentationSentence">
+            {{ stats.presentationSentence }}
+          </div>
+        </b-card-header>
+        <div class="p-2">
+          <div class="d-flex">
+            <Medal
+              v-for="(numberOfPoints, contribution) in points"
+              :key="contribution"
+              :contribution="contribution"
+              :user-level-points="numberOfPoints"
+              small
+            />
+          </div>
+          <div class="clearfix" />
+          <div>
+            {{ stats.numberOfIssues }}
+            {{ $tc("numéro | numéros", stats.numberOfIssues) }}<br />
+            {{ stats.numberOfPublications }}
+            {{ $tc("magazine | magazines", stats.numberOfPublications) }}<br />
+            {{ stats.numberOfCountries }}
+            {{ $tc("pays | pays", stats.numberOfCountries) }}
+          </div>
+          <div v-if="stats.shared === '1'" class="bookcase-link">
+            <b-button
+              size="xs"
+              variant="outline-secondary"
+              target="_blank"
+              :href="r(`/bookcase/show/{username:${stats.username}}`)"
+            >
+              <img
+                style="height: 16px"
+                :src="`${imagePath}/icons/bookcase.png`"
+              />&nbsp;
+              {{ $t("Voir la bibliothèque") }}
+            </b-button>
+          </div>
         </div>
-      </b-card-header>
-      <b-card-body>
-        <div class="d-flex">
-          <Medal
-            v-for="(numberOfPoints, contribution) in points"
-            :key="contribution"
-            :contribution="contribution"
-            :user-level-points="numberOfPoints"
-            small
-          />
-        </div>
-        <div class="clearfix" />
-        <div>
-          {{ stats.numberOfIssues }}
-          {{ $tc("numéro | numéros", stats.numberOfIssues) }}<br />
-          {{ stats.numberOfPublications }}
-          {{ $tc("magazine | magazines", stats.numberOfPublications) }}<br />
-          {{ stats.numberOfCountries }}
-          {{ $tc("pays | pays", stats.numberOfCountries) }}
-        </div>
-        <div v-if="stats.shared === '1'" class="bookcase-link">
-          <b-button
-            size="xs"
-            variant="secondary"
-            target="_blank"
-            :href="r(`/bookcase/show/{username:${stats.username}}`)"
-          >
-            <img
-              style="height: 16px"
-              :src="`${imagePath}/icons/bookcase.png`"
-            />&nbsp;
-            {{ $t("Voir la bibliothèque") }}
-          </b-button>
-        </div>
-      </b-card-body>
-    </b-card>
-  </b-popover>
+      </b-card>
+    </template>
+  </Popper>
 </template>
 
 <script setup>
-import {
-  BButton,
-  BCard,
-  BCardBody,
-  BCardHeader,
-  BPopover,
-} from "bootstrap-vue-3";
+import Popper from "@bperel/vue3-popper-teleport";
+import { BButton, BCard, BCardHeader } from "bootstrap-vue-3";
 
 import { imagePath } from "../composables/imagePath";
 import { l10n } from "../stores/l10n";
@@ -72,7 +76,15 @@ defineProps({
   points: { type: Object, required: true },
   stats: { type: Object, required: true },
 });
-const { r } = l10n();
+const { r } = l10n(),
+  closeDelay = 500,
+  closePopupSoon = () => {
+    setTimeout(() => {
+      isOverPopupText = false;
+    }, closeDelay);
+  };
+let isOverPopup = $ref(false),
+  isOverPopupText = $ref(false);
 </script>
 
 <style scoped lang="scss">
