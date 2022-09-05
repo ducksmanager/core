4ecc5e5cf349170fe6b3c1d3e9656b2fc2fa30b5
Bruno Perel <brunoperel@gmail.com>
2022-09-05 22:40:54 +0200

    Add admin page for POST /edgecreator/edgesprites

---
+++ src/pages/admin/generate-sprites.vue
@@ -0,0 +1,19 @@
+<route lang="yaml">
+meta:
+  layout: bare
+</route>
+
+<template>
+  <a @click="generateSprites()" href="javascript:void(0)">Generate sprites</a>
+</template>
+
+<script setup>
+import axios from 'axios'
+
+let bookstores = $ref(null)
+
+const generateSprites = async ({ id }) => {
+  await axios.post('/edgecreator/edgesprites')
+}
+
+</script>
