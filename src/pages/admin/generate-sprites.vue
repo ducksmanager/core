f1b082e5f5699a0b5ae0bc3c74682862fb1b4c35
Bruno Perel <brunoperel@gmail.com>
2022-09-06 11:06:05 +0200

    Add header

--- src/pages/admin/generate-sprites.vue
+++ src/pages/admin/generate-sprites.vue
@@ -9,11 +9,16 @@ meta:
 
 <script setup>
 import axios from 'axios'
+import Cookies from "js-cookie";
 
 let bookstores = $ref(null)
 
 const generateSprites = async () => {
-  await axios.post('/edgecreator/edgesprites')
+  await axios.post('/edgecreator/edgesprites', {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("token")}`,
+    },
+  })
 }
 
 </script>
