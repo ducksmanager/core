171c9d80520e0a0ee188cf35f3f0f5aff91331e0
Bruno Perel <brunoperel@gmail.com>
2022-09-28 15:09:49 +0200

    Login using a token instead of username/password

--- apps/whattheduck/src/main.ts
+++ apps/whattheduck/src/main.ts
@@ -27,19 +27,6 @@ import axios from "axios";
 
 const pinia = createPinia();
 
-const sha1 = async (str: string) => {
-  const buffer = new TextEncoder().encode(str);
-  return crypto.subtle.digest("SHA-1", buffer).then((hash) => {
-    const hexCodes = [];
-    const view = new DataView(hash);
-    for (let i = 0; i < view.byteLength; i++) {
-      const byte = view.getUint8(i).toString(16).padEnd(2, "0");
-      hexCodes.push(byte);
-    }
-    return hexCodes.join("");
-  });
-};
-
 import * as CordovaSQLiteDriver from "localforage-cordovasqlitedriver";
 import { Storage } from "@ionic/storage";
 import * as localforage from "localforage";
@@ -50,18 +37,13 @@ const storage = new Storage({
 
 storage.defineDriver(CordovaSQLiteDriver).then(async () => {
   await storage.create();
-  await storage.set("name", "Mr. Ionitron");
-  console.log(await storage.keys());
-});
+  await storage.set("token", process.env.VUE_APP_TOKEN);
+  console.log(await storage.get("a"));
 
-sha1(process.env.VUE_APP_USER_PASSWORD).then((passwordHash: string) => {
   axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
-  axios.defaults.headers.common["Authorization"] = `Basic ${btoa(
-    `${process.env.VUE_APP_ROLE_NAME}:${process.env.VUE_APP_ROLE_PASSWORD}`
+  axios.defaults.headers.common["Authorization"] = `Basic ${await storage.get(
+    "token"
   )}`;
-  axios.defaults.headers.common["x-dm-version"] = `1.0.0`;
-  axios.defaults.headers.common["x-dm-user"] = `demo`;
-  axios.defaults.headers.common["x-dm-pass"] = passwordHash;
 
   const app = createApp(App).use(IonicVue).use(router).use(pinia);
 
