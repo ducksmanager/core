2ed93f1909f7220c8d5914a1c2ea345d25ba6545
Bruno Perel <brunoperel@gmail.com>
2022-12-17 20:37:22 +0100

    Hopefully fix alias

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "~/": `${path.resolve(__dirname, "src")}/`,
+      "~prisma_clients/*": `${path.resolve(__dirname, "api/dist/prisma")}/`,
     },
   },
   build: {
