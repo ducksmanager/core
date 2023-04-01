9e372187a82b5198884dff6030a6cd9e6a379fb9
Bruno Perel <brunoperel@gmail.com>
2023-04-01 15:58:22 +0200

    Redirect to Duckguessr after a successful login on DM

--- apps/duckguessr/components/AlertNotConnected.vue
+++ apps/duckguessr/components/AlertNotConnected.vue
@@ -3,8 +3,8 @@
     <b-alert show variant="warning" class="text-center">
       {{ t("You are not connected. You can still play but you won't get any medals.") }}
       <div>
-        <a href="https://ducksmanager.net/login" target="_blank">{{
-          t('Log in on DucksManager then refresh this page to be able to win medals :-)')
+        <a :href="dmUrlWithRedirectToHere" target="_blank">{{
+          t('Log in on DucksManager to be able to win medals :-)')
         }}</a>
       </div>
     </b-alert>
@@ -13,6 +13,8 @@
 
 <script setup lang="ts">
 import { useI18n } from 'nuxt-i18n-composable'
+
+const dmUrlWithRedirectToHere = `https://ducksmanager.net/login?redirect=${process.env.NUXT_URL}`
 
 const { t } = useI18n()
 </script>
