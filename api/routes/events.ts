ff4373fd59b3e93aa0b0847044a32d395221c4a9
Bruno Perel <brunoperel@gmail.com>
2023-05-04 22:51:49 +0200

    Don't show se/WDS in "Recently"

--- api/routes/events.ts
+++ api/routes/events.ts
@@ -145,6 +145,7 @@ const retrieveEdgeCreations = async (): Promise<EdgeCreationEvent[]> =>
                 AND NOT (tp.publicationcode = 'fr/JM' AND tp.issuenumber REGEXP '^[0-9]+$')
                 AND NOT (tp.publicationcode = 'be/MMN')
                 AND NOT (tp.publicationcode = 'it/TL')
+                AND NOT (tp.publicationcode = 'se/WDS')
               GROUP BY tp.ID) as edges_and_collaborators
         group by DATE_FORMAT(creationDate, '%Y-%m-%d %H:00:00'), edges_and_collaborators.users
     `) as EdgeCreationEventRaw[]
