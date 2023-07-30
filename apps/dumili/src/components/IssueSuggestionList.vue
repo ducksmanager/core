29c3a8af0310f383f6e4a837c427db7dba38503e
Bruno Perel <brunoperel@gmail.com>
2023-07-30 12:16:01 +0200

    WIP

--- apps/dumili/src/components/IssueSuggestionList.vue
+++ apps/dumili/src/components/IssueSuggestionList.vue
@@ -1,43 +1,23 @@
 <template>
-  <b-dropdown class="my-2"
-    ><b-dropdown-item
-      v-for="issueSuggestion of issueSuggestions"
-      :key="issueSuggestion.issuecode"
-      class="d-flex"
-      @click="acceptIssueSuggestion(issueSuggestion.issuecode)"
-    >
+  <suggestion-list
+    :suggestions="issueSuggestions"
+    :get-current="() => issue"
+    @select="acceptIssueSuggestion($event as SuggestedIssue)"
+  >
+    <template #item="{ suggestion }: { suggestion: SuggestedIssue }">
       <Issue
-        :publicationcode="issueSuggestion.publicationcode"
-        :issuenumber="issueSuggestion.issuenumber" /><i-bi-lightbulb-fill
-        v-if="issueSuggestion.type === 'ai'"
-        class="ms-2"
-        color="yellow"
-    /></b-dropdown-item>
-    <b-dropdown-divider v-if="issueSuggestions.length" />
-    <b-dropdown-item @click="acceptIssueSuggestion(undefined)"
-      >Numéro inconnu</b-dropdown-item
-    >
-    <b-dropdown-divider />
-    <b-dropdown-item @click="showIssueSelect = true">{{
-      $t("Personnaliser...")
-    }}</b-dropdown-item>
-    <template #button-content>
-      <template v-if="showIssueSelect">{{ $t("Personnaliser...") }}</template>
-      <div v-else class="d-flex">
-        <Issue
-          :publicationcode="issue.publicationcode || null"
-          :issuenumber="issue.issuenumber || null"
-        /><i-bi-lightbulb-fill
-          v-if="issue.type === 'ai'"
-          class="ms-2"
-          color="yellow"
-        /></div
-    ></template>
-  </b-dropdown>
-  <IssueSelect
-    v-if="showIssueSelect"
-    @change="(issuecode) => addCustomIssuecodeToIssueSuggestions(issuecode)"
-  />
+        :publicationcode="suggestion.publicationcode"
+        :issuenumber="suggestion.issuenumber" /></template
+    ><template #unknown>Numéro inconnu</template
+    ><template #customize>{{ $t("Personnaliser...") }}</template>
+    <template #customize-form>
+      <IssueSelect
+        v-if="showIssueSelect"
+        @change="
+          (issuecode) => addCustomIssuecodeToIssueSuggestions(issuecode)
+        "
+    /></template>
+  </suggestion-list>
 </template>
 
 <script lang="ts" setup>
@@ -50,7 +30,9 @@ const showIssueSelect = ref(false);
 const issueDetailsStore = issueDetails();
 
 const issue = computed(() => issueDetailsStore.issue);
-const issueSuggestions = computed(() => issueDetailsStore.issueSuggestions);
+const issueSuggestions = computed(
+  () => issueDetailsStore.issueSuggestions as SuggestedIssue[]
+);
 
 const addCustomIssuecodeToIssueSuggestions = (issuecode: string | null) => {
   if (issuecode) {
@@ -67,12 +49,12 @@ const addCustomIssuecodeToIssueSuggestions = (issuecode: string | null) => {
       type: "custom",
     };
     issueDetailsStore.issueSuggestions.push(userSuggestion);
-    acceptIssueSuggestion(issuecode);
+    acceptIssueSuggestion(userSuggestion);
   }
 };
 
-const acceptIssueSuggestion = (issuecode?: string) => {
-  issueDetailsStore.acceptIssueSuggestion(issuecode);
+const acceptIssueSuggestion = (suggestion?: SuggestedIssue) => {
+  issueDetailsStore.acceptIssueSuggestion(suggestion?.issuecode);
   showIssueSelect.value = false;
 };
 </script>
