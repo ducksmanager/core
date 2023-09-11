375224f76d519b249bb84a03f5cab630a3e9d9c0
Bruno Perel <brunoperel@gmail.com>
2023-09-11 10:46:57 +0200

    Update dependencies

--- apps/dumili/api/index.ts
+++ apps/dumili/api/index.ts
@@ -39,10 +39,11 @@ app.all(/^.+$/, authenticateToken);
 
 app.use(express.json({ limit: "5mb" }));
 
-app.use("/", router());
+router().then((r) => {
+  app.use("/", r);
+  app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
 
-app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
-
-app.listen(port, () => {
-  console.log(`API listening on port ${port}`);
+  app.listen(port, () => {
+    console.log(`API listening on port ${port}`);
+  });
 });
