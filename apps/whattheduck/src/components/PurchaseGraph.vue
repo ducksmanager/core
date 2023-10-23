1bc2dfffbf55ca6b4726d15e19f7cba7c8aa1e7d
Bruno Perel <bruno.perel@firstvet.com>
2023-10-23 22:07:47 +0200

    Init purchase graph

---
+++ apps/whattheduck/src/components/PurchaseGraph.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="wrapper" :style="style || {}">
+    <bar v-if="chartData" :data="chartData" :options="options" />
+  </div>
+</template>
+<script setup lang="ts">
+import {
+  BarController,
+  BarElement,
+  CategoryScale,
+  Chart,
+  ChartOptions,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import dayjs from 'dayjs';
+import { watch } from 'vue';
+import { Bar } from 'vue-chartjs';
+import { useI18n } from 'vue-i18n';
+
+import { coa } from '~web/src/stores/coa';
+import { wtdcollection, purchaseWithStringDate } from '~/stores/wtdcollection';
+import { issue as dm_issue } from '~prisma-clients/client_dm';
+
+const props = defineProps<{
+  since: 'pastYear' | 'allTime';
+  style: Record<string, string>;
+}>();
+
+Chart.register(Legend, CategoryScale, BarElement, LinearScale, BarController, Tooltip, Title);
+
+const hasPublicationNames = ref(false as boolean),
+  purchasesById = ref(null as { [purchaseId: number]: purchaseWithStringDate } | null),
+  options = ref({} as ChartOptions<'bar'>);
+
+const wtdCollectionStore = wtdcollection(),
+  { t } = useI18n(),
+  compareDates = (a: string, b: string) =>
+    dayjs(a === '?' ? '0001-01-01' : a).diff(dayjs(b === '?' ? '0001-01-01' : b)),
+  randomColor = () =>
+    `rgb(${[Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)].join(
+      ',',
+    )})`,
+  getIssueMonth = (issue: dm_issue): string =>
+    getIssueDate(issue).isValid() ? getIssueDate(issue).format('YYYY-MM') : '?',
+  getIssueDate = (issue: dm_issue) =>
+    dayjs((issue.purchaseId && purchasesById.value![issue.purchaseId]?.date) || issue.creationDate),
+  purchases = computed(() => wtdCollectionStore.purchases),
+  publicationNames = computed(() => coa().publicationNames),
+  publicationCodesWithOther = computed(
+    () =>
+      wtdCollectionStore.totalPerPublication &&
+      Object.entries(wtdCollectionStore.totalPerPublication || {})
+        .sort(([, count1], [, count2]) => Math.sign(count2 - count1))
+        .filter((_entry, idx) => idx < 5)
+        .map(([publicationcode]) => publicationcode)
+        .concat(['Other']),
+  ),
+  collectionWithDates = computed(
+    () =>
+      (purchasesById.value &&
+        wtdCollectionStore.collection?.map((issue) => ({
+          ...issue,
+          date: getIssueMonth(issue),
+        }))) ||
+      null,
+  ),
+  labels = computed(
+    () =>
+      collectionWithDates.value &&
+      [...new Set(collectionWithDates.value.map(({ date }) => date))].filter((date) => date).sort(compareDates),
+  ),
+  ready = computed(() => labels.value && hasPublicationNames.value),
+  values = computed(() => {
+    if (!collectionWithDates.value) {
+      return null;
+    }
+    const dateAssoc = labels.value!.reduce(
+      (dates, date) => ({
+        ...dates,
+        [date]: 0,
+      }),
+      {},
+    );
+
+    let accDate: { [label: string]: number } = labels.value!.reduce((acc, value) => ({ ...acc, [value]: 0 }), {});
+    return collectionWithDates.value
+      .sort(({ date: dateA }, { date: dateB }) => compareDates(dateA, dateB))
+      .reduce(
+        (acc, { date, publicationcode: publicationcode }) => {
+          if (!publicationCodesWithOther.value!.includes(publicationcode)) {
+            publicationcode = 'Other';
+          }
+          if (!acc[publicationcode]) {
+            acc[publicationcode] = { ...dateAssoc };
+          }
+          acc[publicationcode][date]++;
+          accDate[date]++;
+          return acc;
+        },
+        {} as { [publicationcode: string]: { [date: string]: number } },
+      );
+  }),
+  datasets = computed(() =>
+    !(ready.value && collectionWithDates.value && values.value)
+      ? null
+      : Object.keys(values.value).map((publicationcode) => {
+          let data = values.value![publicationcode];
+          data = labels.value!.reduce(
+            (acc, currentDate) => ({
+              ...acc,
+              [currentDate]: labels
+                .value!.filter((_, idx) => idx <= labels.value!.indexOf(currentDate))
+                .reduce((sum, date) => sum + data[date], 0),
+            }),
+            {},
+          );
+          return {
+            data: Object.values(data),
+            label:
+              publicationcode === 'Other' ? t('Autres') : publicationNames.value[publicationcode] || publicationcode,
+            backgroundColor: publicationcode === 'Other' ? '#aaa' : randomColor(),
+          };
+        }),
+  ),
+  chartData = computed(() =>
+    !(datasets.value && labels.value)
+      ? null
+      : {
+          datasets: datasets.value!.map((value) =>
+            props.since === 'allTime'
+              ? value
+              : { ...value, data: value.data.filter((_, idx) => idx > value.data.length - 12) },
+          ),
+          labels: labels.value.filter((_, idx) => (props.since === 'allTime' ? true : idx > labels.value!.length - 12)),
+        },
+  );
+
+watch(
+  () => publicationCodesWithOther.value,
+  async (newValue) => {
+    if (newValue) {
+      await coa().fetchPublicationNames(newValue.filter((value) => value !== 'Other'));
+      hasPublicationNames.value = true;
+    }
+  },
+  { immediate: true },
+);
+
+watch(
+  () => purchases.value,
+  (newValue) => {
+    if (newValue) {
+      purchasesById.value = newValue.reduce(
+        (acc, purchase) => ({
+          ...acc,
+          [purchase.id]: purchase,
+        }),
+        {} as { [purchaseId: number]: purchaseWithStringDate },
+      );
+    }
+  },
+  { immediate: true },
+);
+
+watch(
+  () => datasets.value && labels.value,
+  (newValue) => {
+    if (newValue) {
+      options.value = {
+        animation: {
+          duration: 0,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            stacked: true,
+            ticks: {
+              color: 'white',
+            },
+          },
+          y: {
+            stacked: true,
+            ticks: {
+              color: 'white',
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              color: 'white',
+            },
+          },
+          tooltip: {
+            enabled: true,
+            callbacks: {
+              beforeTitle: ([tooltipItem]) =>
+                (tooltipItem.label !== '?'
+                  ? `${t('Taille de la collection pour le mois')} ${tooltipItem.label}`
+                  : t("Numéros sans dates d'achat")) + '\n',
+              title: ([tooltipItem]) => chartData.value!.datasets[tooltipItem.datasetIndex].label,
+              label: (tooltipItem) => tooltipItem.raw as string,
+              footer: ([tooltipItem]) =>
+                [
+                  t('Tous magazines'),
+                  datasets.value!.reduce((acc, dataset) => acc + dataset.data[tooltipItem.dataIndex], 0),
+                ].join('\n'),
+            },
+          },
+        },
+      };
+    }
+  },
+  { immediate: true },
+);
+
+wtdCollectionStore.loadCollection();
+wtdCollectionStore.loadPurchases();
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  height: 100% !important;
+
+  > div,
+  :deep(canvas) {
+    width: 100% !important;
+    height: 100% !important;
+  }
+}
+</style>
--- apps/whattheduck/src/views/Stats.vue
+++ apps/whattheduck/src/views/Stats.vue
@@ -9,7 +9,7 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true" v-if="wtdCollectionStore.collection">
-      <ion-row style="height: 25%" class="ion-text-center">
+      <ion-row style="height: 25vh" class="ion-text-center">
         <ion-row class="ion-text-center" style="height: 75%">
           <ion-col size="4" class="text-big">{{ wtdCollectionStore.ownedCountries.length }}</ion-col
           ><ion-col size="4" class="text-big">{{ wtdCollectionStore.ownedPublications.length }}</ion-col
@@ -18,8 +18,9 @@
         <ion-row class="ion-text-center" style="height: 25%">
           <ion-col size="4">{{ t('pays') }}</ion-col
           ><ion-col size="4">{{ t('magazines') }}</ion-col
-          ><ion-col size="4"
-            >{{ t('numéros') }}<br /><small>{{
+          ><ion-col size="4" style="flex-direction: column"
+            ><div>{{ t('numéros') }}</div>
+            <small>{{
               t('dont {copies} double|dont {copies} doubles', {
                 copies: wtdCollectionStore.total! - wtdCollectionStore.totalUniqueIssues,
               })
@@ -27,43 +28,45 @@
           </ion-col></ion-row
         >
       </ion-row>
-      <ion-row style="height: 50%" class="ion-padding-vertical">
-        <ion-col size="12" class="ion-justify-content-center" style="display: block">
-          <ion-title class="ion-text-center" style="max-height: 20%">{{ t('Etats des numéros') }}</ion-title>
-          <StatsComponent
-            :link-to-collection-if-no-issue="false"
+      <ion-row style="height: 50vh" class="ion-padding-vertical">
+        <ion-col size="12" class="ion-justify-content-around" style="flex-direction: column">
+          <ion-text style="height: 20%" class="text-medium">{{ t('Etats des numéros') }}</ion-text>
+          <ConditionsComponent
+            :style="{ width: '100%', height: '80%' }"
             :conditions="conditionsWithoutMissing"
             :number-per-condition="numberPerCondition"
         /></ion-col>
       </ion-row>
-      <ion-row style="height: 25%">
-        <ion-col size="12" class="ion-text-center ion-justify-content-center" style="flex-direction: column">
-          <ion-title>{{ t('Valeur de la collection') }}</ion-title>
+      <ion-row style="height: 25vh">
+        <ion-col size="12" class="ion-text-center ion-justify-content-around" style="flex-direction: column">
+          <ion-text class="text-medium">{{ t('Valeur de la collection') }}</ion-text>
           <ion-text class="text-big">{{ wtdCollectionStore.quotationSum }}&euro;</ion-text>
-          <template v-if="wtdCollectionStore.highestQuotedIssue">
+          <template v-if="highestQuotedIssue">
             <ion-text>{{ t('Numéro le plus côté :') }}</ion-text>
             <ion-text>
-              <Condition :value="getConditionText(wtdCollectionStore.highestQuotedIssue.condition)" />
-              {{ coaStore.publicationNames[wtdCollectionStore.highestQuotedIssue.publicationcode] }}
-              {{ wtdCollectionStore.highestQuotedIssue.issuenumber }}</ion-text
+              <Condition :value="getConditionText(highestQuotedIssue.condition)" />
+              {{ coaStore.publicationNames[highestQuotedIssue.publicationcode] }}
+              {{ highestQuotedIssue.issuenumber }}</ion-text
             ></template
           >
+          <ion-text v-else>{{ t('Vous ne possédez pas de numéro côté.') }}</ion-text>
         </ion-col>
       </ion-row>
-      <ion-row style="height: 50%" class="ion-text-center">
-        <ion-title>{{ t('Progression de la collection') }}</ion-title>
-        <ion-row style="height: initial">
-          <ion-col v-for="{ title, value } of collectionProgressionGraphTypes">
-            <ion-button
-              expand="block"
-              :fill="value === currentCollectionProgressionGraphType ? 'solid' : 'outline'"
-              @click="currentCollectionProgressionGraphType = value"
-              >{{ title }}</ion-button
-            >
-          </ion-col></ion-row
-        >
-
-        <ion-col size="12"> TODO </ion-col>
+      <ion-row style="height: calc(100vh - 50px)" class="ion-align-items-center">
+        <ion-col size="12" class="ion-text-center ion-justify-content-around" style="flex-direction: column">
+          <ion-text :style="{ height: '60px' }" class="text-medium">{{ t('Progression de la collection') }}</ion-text>
+          <ion-row :style="{ height: '60px' }">
+            <ion-col v-for="{ title, value } of collectionProgressionGraphTypes">
+              <ion-button
+                expand="block"
+                :fill="value === currentCollectionProgressionGraphType ? 'solid' : 'outline'"
+                @click="currentCollectionProgressionGraphType = value"
+                >{{ title }}</ion-button
+              >
+            </ion-col></ion-row
+          >
+          <PurchaseGraph :since="currentCollectionProgressionGraphType" :style="{ height: 'calc(100% - 140px)' }"
+        /></ion-col>
       </ion-row>
     </ion-content>
   </ion-page>
@@ -76,7 +79,7 @@ import { components } from '~web';
 import { coa } from '~web/src/stores/coa';
 import { getConditionText } from '~/composables/useCondition';
 
-const StatsComponent = components['Conditions'];
+const ConditionsComponent = components['Conditions'];
 const { t } = useI18n();
 
 type GraphType = 'pastYear' | 'allTime';
@@ -120,11 +123,19 @@ ion-col {
   }
 }
 
+ion-title {
+  padding-inline: 0;
+}
+
 ion-button {
   width: 100%;
 }
 
+.text-medium {
+  font-size: 24px;
+}
+
 .text-big {
-  font-size: 10vh;
+  font-size: 52px;
 }
 </style>
