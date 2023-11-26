d5ba1416147e1e1554ec7765bd8c1f90a757aa25
Bruno Perel <bruno.perel@firstvet.com>
2023-11-26 19:46:05 +0100

    Create Appflow config

--- apps/whattheduck/capacitor.config.ts
+++ apps/whattheduck/capacitor.config.ts
@@ -4,9 +4,6 @@ const config: CapacitorConfig = {
   appId: 'net.ducksmanager.whattheduck2',
   appName: 'What The Duck',
   webDir: 'dist',
-  server: {
-    url: 'http://localhost:8005',
-  },
 };
 
 export default config;
