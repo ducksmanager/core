f898b3f2477ffaabf442dcf5adf98f0fb8e66f11
Bruno Perel <brunoperel@gmail.com>
2024-04-24 23:09:12 +0200

    Lint

--- packages/api/services/collection/user/index.ts
+++ packages/api/services/collection/user/index.ts
@@ -1,9 +1,7 @@
 import { Socket } from "socket.io";
 
 import { prismaDm } from "~/prisma";
-import { exclude } from "~dm-types/exclude";
 import PresentationSentenceRequested from "~emails/presentation-sentence-requested";
-import { user } from "~prisma-clients/client_dm";
 import { generateAccessToken, getHashedPassword } from "~services/auth/util";
 
 import Events from "../types";
