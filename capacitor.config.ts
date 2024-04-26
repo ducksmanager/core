a92bb09abaf27a726ca372f8f22ac6da3faa84e6
Bruno Perel <brunoperel@gmail.com>
2024-04-26 14:16:48 +0200

    Copy capacitor config from wtd submodule

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "d5426d00",
+  appName: "What The Duck",
+  webDir: "dist",
+};
+
+export default config;
