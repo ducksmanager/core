598d7c514f6593f4ef0df56fc5d49f27ae2560fc
Bruno Perel <brunoperel@gmail.com>
2024-04-26 18:58:02 +0200

    Fix configs

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "d5426d00",
-  appName: "What The Duck",
-  webDir: "dist",
-};
-
-export default config;
