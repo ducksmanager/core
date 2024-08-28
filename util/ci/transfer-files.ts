4b5d31b87a570441a83e7f0c427b1eca7fa9a844
Bruno Perel <brunoperel@gmail.com>
2024-08-28 17:38:48 +0200

    WIP calculate changed apps

--- util/ci/transfer-files.ts
+++ util/ci/transfer-files.ts
@@ -31,10 +31,10 @@ for (const transfer of transfers) {
         const localFile = `../../${(targetFileIsRemote ? sourceFile : targetFile)}`;
         if (targetFileIsRemote) {
             console.log(`Uploading ${localFile} to ${remoteFile}`);
-            await sftp.put(sourceFile, targetFile);
+            await sftp.put(sourceFile, remoteFile);
         } else {
             console.log(`Downloading ${remoteFile} to ${localFile}`);
-            await sftp.get(sourceFile, targetFile);
+            await sftp.get(sourceFile, remoteFile);
         }
     } catch (error) {
         console.error("Error:", error.message);
