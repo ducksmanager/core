aa7015fca3930a100b35ceaed81abbc62b332131
Bruno Perel <brunoperel@gmail.com>
2024-09-09 15:22:04 +0200

    cover-updater: Fix envvars

--- apps/cover-updater/scripts/import-covers.sh
+++ apps/cover-updater/scripts/import-covers.sh
@@ -3,7 +3,6 @@
 COVER_LIST_EXPORT_FILE=/home/covers.csv
 
 echo "Exporting covers from the COA database..."
-mysql -h ${MYSQL_COA_HOST} -uroot -p${MYSQL_COA_PASSWORD} ${MYSQL_COA_DATABASE} < /home/scripts/sql/get-coa-covers.sql | sed 's/\t/,/g' > ${COVER_LIST_EXPORT_FILE} && \
-\
-echo "Importing covers into the covers database..." && \
-mysql -uroot -p${MYSQL_COVER_INFO_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} --local-infile=1 ${MYSQL_COVER_INFO_DATABASE} -e "LOAD DATA LOCAL INFILE \"${COVER_LIST_EXPORT_FILE}\" IGNORE INTO TABLE covers FIELDS TERMINATED BY ',' IGNORE 1 LINES (issuecode,sitecode,url);"
+mysql -h ${MYSQL_COA_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} ${MYSQL_COA_DATABASE} </home/scripts/sql/get-coa-covers.sql | sed 's/\t/,/g' >${COVER_LIST_EXPORT_FILE} &&
+    echo "Importing covers into the covers database..." &&
+    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} --local-infile=1 ${MYSQL_COVER_INFO_DATABASE} -e "LOAD DATA LOCAL INFILE \"${COVER_LIST_EXPORT_FILE}\" IGNORE INTO TABLE covers FIELDS TERMINATED BY ',' IGNORE 1 LINES (issuecode,sitecode,url);"
--- apps/cover-updater/scripts/process-covers.sh
+++ apps/cover-updater/scripts/process-covers.sh
@@ -8,28 +8,28 @@ LIMIT=${LIMIT_DEFAULT}
 IMAGES_PER_GROUP=${IMAGES_PER_GROUP_DEFAULT}
 THREADS=${THREADS_DEFAULT}
 
-usage(){
-  echo "Usage: $0 [--help | [--threads=<Number of threads>]|$THREADS_DEFAULT [--images-per-group=<Number of images processed in a row>]|$IMAGES_PER_GROUP_DEFAULT [--limit=<Number of images to process>]|$LIMIT_DEFAULT"
-  exit 1
+usage() {
+    echo "Usage: $0 [--help | [--threads=<Number of threads>]|$THREADS_DEFAULT [--images-per-group=<Number of images processed in a row>]|$IMAGES_PER_GROUP_DEFAULT [--limit=<Number of images to process>]|$LIMIT_DEFAULT"
+    exit 1
 }
 
 deleteNonIndexedCovers() {
-  coversInDbButNotInIndex=$(comm -23 \
-    <(mysql -uroot -p${MYSQL_COVER_INFO_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} -BN ${MYSQL_COVER_INFO_DATABASE} -e \
-      "select coverid from cover_imports where import_error is null" | sed 's/[^0-9]//g' | sort) \
-    <(curl -X GET http://${PASTEC_HOST}:${PASTEC_PORT}/index/imageIds | head | jq '.image_ids' | grep -Po '[\d]+' | sort))
+    coversInDbButNotInIndex=$(comm -23 \
+        <(mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} -BN ${MYSQL_COVER_INFO_DATABASE} -e \
+            "select coverid from cover_imports where import_error is null" | sed 's/[^0-9]//g' | sort) \
+        <(curl -X GET http://${PASTEC_HOST}:${PASTEC_PORT}/index/imageIds | head | jq '.image_ids' | grep -Po '[\d]+' | sort))
 
-  # Delete referenced covers that are not in the index, by chunks of 20
-  echo "$coversInDbButNotInIndex" | tr ' ' '\n' |paste -sd',,,,,,,,,,,,,,,,,,,,\n' | while read -r chunk; do
-    nonIndexedCoversQuery="DELETE FROM cover_imports where coverid in ($chunk);"
-    mysql -uroot -p${MYSQL_COVER_INFO_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} ${MYSQL_COVER_INFO_DATABASE} -e "$nonIndexedCoversQuery"
-  done
+    # Delete referenced covers that are not in the index, by chunks of 20
+    echo "$coversInDbButNotInIndex" | tr ' ' '\n' | paste -sd',,,,,,,,,,,,,,,,,,,,\n' | while read -r chunk; do
+        nonIndexedCoversQuery="DELETE FROM cover_imports where coverid in ($chunk);"
+        mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} ${MYSQL_COVER_INFO_DATABASE} -e "$nonIndexedCoversQuery"
+    done
 
 }
 
 processCovers() {
     local thread_id=$1
-    local offset=$((${thread_id}*${IMAGES_PER_GROUP}))
+    local offset=$((${thread_id} * ${IMAGES_PER_GROUP}))
 
     echo "[Thread $thread_id] Fetching ${IMAGES_PER_GROUP} covers, cover id offset ${processed_covers}"
 
@@ -39,15 +39,15 @@ processCovers() {
     rm -f ${SHM_FILE} && touch ${SHM_FILE}
 
     coverquery=$(getCoverQuery ${IMAGES_PER_GROUP} ${offset})
-    mysql -uroot -p${MYSQL_COVER_INFO_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} ${MYSQL_COVER_INFO_DATABASE} -se "$coverquery" > ${RESULTS_FILE}
+    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} ${MYSQL_COVER_INFO_DATABASE} -se "$coverquery" >${RESULTS_FILE}
 
     if [ -s ${RESULTS_FILE} ]; then
         while read id fullurl; do
             processImage ${id} ${fullurl} ${thread_id} &
-        done < ${RESULTS_FILE}
+        done <${RESULTS_FILE}
         wait
 
-        mysql -uroot -p${MYSQL_COVER_INFO_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} ${MYSQL_COVER_INFO_DATABASE} -e "$(< ${SHM_FILE})"
+        mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_COVER_INFO_HOST} ${MYSQL_COVER_INFO_DATABASE} -e "$(<${SHM_FILE})"
     else
         echo "[Thread $thread_id] File is empty, no more covers"
     fi
@@ -56,26 +56,26 @@ processCovers() {
 getCoverQuery() {
     local images_per_group=$1
     local offset=$2
-    echo `cat ${DIR}/sql/get-imported-covers.sql | sed "s/_IMAGES_PER_GROUP_/$images_per_group/" | sed "s/_COVER_OFFSET_/$offset/"`
+    echo $(cat ${DIR}/sql/get-imported-covers.sql | sed "s/_IMAGES_PER_GROUP_/$images_per_group/" | sed "s/_COVER_OFFSET_/$offset/")
 }
 
 getCoverLogInsertSuccessQuery() {
     local id=$1
     local datetime=$(date +'%F %T')
-    echo `cat ${DIR}/sql/log-cover-import-success.sql | sed "s/_COVERID_/$id/" | sed "s/_IMPORT_DATE_/$datetime/"`
+    echo $(cat ${DIR}/sql/log-cover-import-success.sql | sed "s/_COVERID_/$id/" | sed "s/_IMPORT_DATE_/$datetime/")
 }
 
 getCoverLogInsertErrorQuery() {
     local id=$1
     local error=$2
-    echo `cat ${DIR}/sql/log-cover-import-error.sql | sed "s/_COVERID_/$id/" | sed "s/_ERROR_MESSAGE_/$error/"`
+    echo $(cat ${DIR}/sql/log-cover-import-error.sql | sed "s/_COVERID_/$id/" | sed "s/_ERROR_MESSAGE_/$error/")
 }
 
 downloadPicture() {
     local url=$1
     local output=$2
-    if wget -q "${url}" -O ${output} > /dev/null; then
-        if [[ "$( wc -m ${output} | awk '{print $1}' )" -lt 200 ]]; then
+    if wget -q "${url}" -O ${output} >/dev/null; then
+        if [[ "$(wc -m ${output} | awk '{print $1}')" -lt 200 ]]; then
             return -1
         else
             return 0
@@ -88,7 +88,7 @@ downloadPicture() {
 addQueryToSqlList() {
     SHM_FILE=${SHM_DIR}/coversqls-$1
     str=$2
-    echo -n "$str" >>${SHM_FILE};
+    echo -n "$str" >>${SHM_FILE}
 }
 
 processImage() {
@@ -97,7 +97,7 @@ processImage() {
     local thread_id=$3
 
     output=${DOWNLOAD_DIR_TMP}/cover_${id}.jpg
-    fullurl=$(echo ${fullurl}|tr -d '\r'|tr -d '\n')
+    fullurl=$(echo ${fullurl} | tr -d '\r' | tr -d '\n')
     fullurlHr="https://inducks.org/hr.php?normalsize=1&image="${fullurl}
     log="\n[Thread $thread_id] id: $id, fullurl: $fullurl, fullurlHr: $fullurlHr\n"
 
@@ -130,41 +130,40 @@ processImage() {
 
 SHM_DIR=/dev/shm
 
-while :
-do
+while :; do
     case $1 in
-        --threads=*)
-            THREADS=${1#*=}
-            shift
-            ;;
-        --images-per-group=*)
-            IMAGES_PER_GROUP=${1#*=}
-            shift
-            ;;
-        --limit=*)
-            LIMIT=${1#*=}
-            shift
-            ;;
-        --help)
-            usage
-            exit
-            ;;
-        --) # End of all options
-            shift
-            break
-            ;;
-        -*)
-            echo "FATAL: Unknown option (ignored): $1" >&2
-            usage
-            shift
-            ;;
-        *)  # no more options. Stop while loop
-            break
-            ;;
+    --threads=*)
+        THREADS=${1#*=}
+        shift
+        ;;
+    --images-per-group=*)
+        IMAGES_PER_GROUP=${1#*=}
+        shift
+        ;;
+    --limit=*)
+        LIMIT=${1#*=}
+        shift
+        ;;
+    --help)
+        usage
+        exit
+        ;;
+    --) # End of all options
+        shift
+        break
+        ;;
+    -*)
+        echo "FATAL: Unknown option (ignored): $1" >&2
+        usage
+        shift
+        ;;
+    *) # no more options. Stop while loop
+        break
+        ;;
     esac
 done
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOWNLOAD_DIR_TMP=/tmp/download_tmp
 
 mkdir -p ${DOWNLOAD_DIR_TMP}
@@ -175,16 +174,16 @@ no_more_covers=false
 
 deleteNonIndexedCovers
 
-while [ ${processed_covers} -lt ${LIMIT} ] && [ ${no_more_covers} = "false" ] ; do
-    for ((thread_id=0; thread_id < THREADS; thread_id++)); do
+while [ ${processed_covers} -lt ${LIMIT} ] && [ ${no_more_covers} = "false" ]; do
+    for ((thread_id = 0; thread_id < THREADS; thread_id++)); do
         processCovers ${thread_id} &
     done
     wait
 
-    for ((thread_id=0; thread_id < THREADS; thread_id++)); do
+    for ((thread_id = 0; thread_id < THREADS; thread_id++)); do
         RESULTS_FILE=${DOWNLOAD_DIR_TMP}/results-${thread_id}.txt
         if [ -s ${RESULTS_FILE} ]; then
-            new_processed_covers=`grep -o 'Downloaded' ${SHM_DIR}/coversqls-$thread_id  | wc -l`
+            new_processed_covers=$(grep -o 'Downloaded' ${SHM_DIR}/coversqls-$thread_id | wc -l)
             processed_covers=$(($processed_covers + $new_processed_covers))
         else
             no_more_covers=true
