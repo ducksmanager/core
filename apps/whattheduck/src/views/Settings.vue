4cf3237b71711956c0c719aad9f018b92f3fc761
Bruno Perel <brunoperel@gmail.com>
2024-11-07 19:27:51 +0100

    whattheduck: Add navigation drawer button on settings page

--- apps/whattheduck/src/views/Settings.vue
+++ apps/whattheduck/src/views/Settings.vue
@@ -2,6 +2,9 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button color="primary" />
+        </ion-buttons>
         <ion-title>{{ t('Paramètres') }}</ion-title>
       </ion-toolbar>
     </ion-header>
