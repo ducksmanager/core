d5f43e300bd637d9881f52b4d708aa5f23528f9b
Bruno Perel <brunoperel@gmail.com>
2025-02-11 14:12:01 +0100

    lint

--- packages/api/services/coa/issue-details/index.ts
+++ packages/api/services/coa/issue-details/index.ts
@@ -117,7 +117,7 @@ export const getCoverUrls = async (issuecodes: string[]) => {
       };
     }
 
-    const urlPrefix = /^\d/.test(coverEntryUrl.url!)
+    const urlPrefix = /^\d/.test(coverEntryUrl.url)
       ? "webusers/webusers"
       : coverEntryUrl.url!.startsWith("webusers")
         ? "webusers"
