62aa2f558afba9758b0ed05ee91efd62d5bc8d55
Bruno Perel <brunoperel@gmail.com>
2025-02-11 17:04:50 +0100

    group-by: Support grouping arrays of strings

--- util/group-by/index.ts
+++ util/group-by/index.ts
@@ -1,20 +1,34 @@
+type NestedKeyOf<T> = {
+  [K in keyof T & string]: T[K] extends object
+    ? `${K}` | `${K}.${NestedKeyOf<T[K]>}`
+    : `${K}`;
+}[keyof T & string];
+
+type GetNestedType<T, P extends string> = P extends keyof T
+  ? T[P]
+  : P extends `${infer K}.${infer R}`
+    ? K extends keyof T
+      ? GetNestedType<T[K], R>
+      : never
+    : never;
+
 type GroupByValueType<
   T,
   V extends
-    | undefined
+    | null
     | "[]"
-    | (keyof T & string)
-    | `${keyof T & string}[]` = undefined,
-> = V extends undefined
+    | NestedKeyOf<T>
+    | `${NestedKeyOf<T>}[]` = null,
+> = V extends null
   ? T
   : V extends "[]"
     ? T[]
     : V extends `${infer U}[]`
-      ? U extends keyof T
-        ? T[U][]
+      ? U extends NestedKeyOf<T>
+        ? GetNestedType<T, U>[]
         : never
-      : V extends keyof T
-        ? T[V]
+      : V extends NestedKeyOf<T>
+        ? GetNestedType<T, V>
         : never;
 
 declare global {
@@ -22,12 +36,14 @@ declare global {
     /**
      * Groups the elements of an array by a specified field name.
      *
-     * @param fieldName - The field name to group by.
+     * @param fieldName - The field name to group by. If equal to null and the array to group by is an array of strings, the keys of the output array will be the values of the input array. Implies that the valueFieldName must be set to '[]' or null.
      * @param valueFieldName - Optional. The field name to use as the value in the grouped result.
-     *                         If not provided, the entire object will be used as the value.
+     *                         If set to 'myField.myOtherField', the value of each object at myField.myOtherField will be used as the value. Only one level of nesting is supported.
      *                         If set to '[]', an array of objects will be used as the value.
      *                         If set to 'myField[]', an array of values of myField will be used as the value.
-     * @param mapperFn - Optional. If set, each value will be passed to the function and the result will be used as the value or an array of values (depending on the value of valueFieldName).
+     *                         If set to 'myField.myOtherField[]', an array of values of myField.myOtherField will be used as the value. Only one level of nesting is supported.
+     *                         If not provided or null, the entire object will be used as the value.
+     * @param mapperFn - Optional. If set, each value (calculated from the valueFieldName parameter) and its index will be mapped using this function.
      * @returns An object with the grouped elements.
      * @example
      * // returns { John: { id: 3, name: 'John' }, Jane: { id: 2, name: 'Jane' } }
@@ -53,15 +69,30 @@ declare global {
      * // returns { John: [3, 9], Jane: [6] }
      * [{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }, { id: 3, name: 'John' }]
      *      .groupBy('name', 'id[]', (id) => id * 3);
+     *
+     * @example
+     * // returns { John: 1, Jane: 2 }
+     * [{ id: {"internal": 1}, name: 'John' }, { id: {"internal": 2}, name: 'Jane' }, { id: {"internal": 3}, name: 'John' }]
+     *      .groupBy('name', 'id.internal');
+     *
+     * @example
+     * // returns { John: [1, 3], Jane: [2] }
+     * [{ id: {"internal": 1}, name: 'John' }, { id: {"internal": 2}, name: 'Jane' }, { id: {"internal": 3}, name: 'John' }]
+     *      .groupBy('name', 'id.internal[]');
+     *
+     * @example
+     * // returns { John: {name: 'John', id: 1}, Jane: {name: 'Jane', id: 2} }
+     * ['John', 'Jane']
+     *      .groupBy(null, null, (name, index) => ({ name, id: index+1 }));
      */
     groupBy<
-      K extends keyof T & string,
+      K extends "" | (keyof T & (string | number)),
       V extends
-        | undefined
+        | null
         | "[]"
-        | (keyof T & string)
-        | `${keyof T & string}[]` = undefined,
-      R = any,
+        | NestedKeyOf<T>
+        | `${NestedKeyOf<T>}[]` = null,
+      R = GroupByValueType<T, V>,
     >(
       fieldName: K,
       valueFieldName?: V,
@@ -74,28 +105,50 @@ declare global {
         index: number
       ) => R
     ): Record<
-      T[K] & (string | number),
-      typeof mapperFn extends undefined ? GroupByValueType<T, V> : R
+      K extends ""
+        ? T & string
+        : T[K & (keyof T & (string | number))] & (string | number),
+      R
     >;
   }
 }
 
+const getNestedValue = <T, P extends string>(
+  object: T,
+  path: P
+): GetNestedType<T, P> =>
+  path
+    .split(".")
+    .reduce((acc, key) => (acc as any)[key], object) as GetNestedType<T, P>;
+
 Array.prototype.groupBy = function (fieldName, mapper, mapperFn) {
   return this.reduce((acc, object, idx) => {
-    const key = object[fieldName];
+    const key = fieldName === "" ? object : object[fieldName];
     if (mapper === "[]" || mapper?.endsWith("[]")) {
       if (!acc[key]) {
         acc[key] = [];
       }
+    }
+    if (fieldName === "") {
+      const value = (mapperFn || ((val) => val))(object, idx);
+      if (mapper === "[]") {
+        acc[key].push(value);
+      } else {
+        acc[key] = value;
+      }
+    }
+    if (mapper === "[]" || mapper?.endsWith("[]")) {
       acc[key].push(
         (mapperFn || ((value) => value))(
-          mapper === "[]" ? object : object[mapper.slice(0, -"[]".length)],
+          mapper === "[]"
+            ? object
+            : getNestedValue(object, mapper.slice(0, -"[]".length)),
           idx
         )
       );
     } else {
       acc[key] = (mapperFn || ((value) => value))(
-        mapper ? object[mapper] : object,
+        mapper ? getNestedValue(object, mapper) : object,
         idx
       );
     }
--- util/group-by/tests/group-by.test.ts
+++ util/group-by/tests/group-by.test.ts
@@ -68,10 +68,32 @@ describe("Array.groupBy", () => {
   });
 
   test("groups by field name with no specific value and a callback", () => {
-    const result = testData.groupBy("name", undefined, ({id}) => id * 3);
+    const result = testData.groupBy("name", null, ({id}) => id * 3);
+    
     expect(result).toEqual({
       John: 9,
       Jane: 6,
     });
+  });
+
+  test("groups by field name using a sub-key", () => {
+    const testDataWithSubKey = [{name: "John", "myKey": {"subKey": "myValue"}}, {name: "Jane", "myKey": {"subKey": "myOtherValue"}}]
+    const result = testDataWithSubKey.groupBy("name", "myKey.subKey")
+    expect(result).toEqual({
+      John: "myValue",
+      Jane: "myOtherValue",
+    });
+  });
+
+  test("groups by field name using a sub-key array", () => {
+    const testDataWithSubKey = [{name: "John", "myKey": {"subKey": "myValue"}}, {name: "John", "myKey": {"subKey": "myOtherValue"}}]
+    const result = testDataWithSubKey.groupBy("name", "myKey.subKey[]")
+    expect(result).toEqual({John: ["myValue", 'myOtherValue']});
+  });
+
+  test("groups by field name using an array of strings and a callback", () => {
+    const testDataWithSubKey = ["John", "Jane"]
+    const result = testDataWithSubKey.groupBy('', null, (name, index) => ({ name, id: index+1 }));
+    expect(result).toEqual({ John: {name: 'John', id: 1}, Jane: {name: 'Jane', id: 2} });
   });
 });
