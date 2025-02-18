3f0c60a14f0a80b6bea0df72f07d7358a5de3b46
Bruno Perel <brunoperel@gmail.com>
2025-02-18 21:59:18 +0100

    whattheduck: Fix assertion

--- apps/whattheduck/src/views/Search.vue
+++ apps/whattheduck/src/views/Search.vue
@@ -123,7 +123,7 @@ watchDebounced(
         issues: story.issues.map(({ part, estimatedpanels, total_estimatedpanels, ...issue }) => ({
           ...issue,
           partInfo: { part, estimatedpanels, total_estimatedpanels },
-          collectionIssues: (collectionIssuesByIssuecode.value[issue.issuecode] || []).map((collectionIssue) => ({
+          collectionIssues: (collectionIssuesByIssuecode.value?.[issue.issuecode] || []).map((collectionIssue) => ({
             condition: collectionIssue.condition,
           })),
         })),
