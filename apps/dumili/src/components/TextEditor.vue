41443cd5889d311a7b07212eafecea1ba154a640
Bruno Perel <brunoperel@gmail.com>
2025-03-06 11:42:45 +0200

    dumili: Add h3

--- apps/dumili/src/components/TextEditor.vue
+++ apps/dumili/src/components/TextEditor.vue
@@ -87,6 +87,7 @@ const issuecode = computed(
 const issueRow = computed(() => ({
   issuecode: issuecode.value,
   details: [
+    "h3",
     ...[indexation.value!.price ? [`[price:${indexation.value!.price}]`] : []],
     ...[
       indexation.value!.releaseDate
