449405686a9ccc808ad2ed757efafd58c7f553cc
Bruno Perel <brunoperel@gmail.com>
2025-03-14 16:22:24 +0100

    Revert "api: Hopefully fix upsert"
    This reverts commit 02ff8109aaab60b107585ba583e6bc839b9f993d.

--- packages/api/services/collection/issues/index.ts
+++ packages/api/services/collection/issues/index.ts
@@ -320,12 +320,27 @@ const addOrChangeCopies = async (
         areToRead[copyNumber] !== undefined ? areToRead[copyNumber] : false,
       purchaseId: purchaseIds[copyNumber] || -2,
     };
+    console.log('upsert', {
+      create: {
+        ...common,
+        country: "",
+        magazine: "",
+        issuenumber: "",
+        issuecode,
+        userId,
+        creationDate: new Date(),
+      },
+      update: common,
+      where: {
+        id: issueId || 0,
+      },
+    })
     return prismaDm.issue.upsert({
       create: {
         ...common,
-        country: "?",
-        magazine: "?",
-        issuenumber: "?",
+        country: "",
+        magazine: "",
+        issuenumber: "",
         issuecode,
         userId,
         creationDate: new Date(),
