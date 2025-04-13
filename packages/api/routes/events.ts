34a37e301de89b5fdbc18a196f557f41eb836820
Bruno Perel <brunoperel@gmail.com>
2025-04-13 11:46:42 +0200

    api: Retrieve edge creation events using their issue code

--- packages/api/routes/events.ts
+++ packages/api/routes/events.ts
@@ -144,17 +144,18 @@ const retrieveEdgeCreations = async (): Promise<EdgeCreationEvent[]> =>
                    )), ']')                 AS edges,
                UNIX_TIMESTAMP(creationDate) AS timestamp,
                users
-        from (SELECT tp.publicationcode,
-                     tp.issuenumber,
+        from (SELECT i.publicationcode,
+                     i.issuenumber,
                      tp.dateajout                       AS creationDate,
                      GROUP_CONCAT(DISTINCT tpc.ID_user) AS users
               FROM dm.tranches_pretes tp
                        INNER JOIN dm.users_contributions tpc ON tpc.ID_tranche = tp.ID
+                       INNER JOIN coa.inducks_issue i USING (issuecode)
               WHERE tp.dateajout > DATE_ADD(NOW(), INTERVAL -1 MONTH)
-                AND NOT (tp.publicationcode = 'fr/JM' AND tp.issuenumber REGEXP '^[0-9]+$')
-                AND NOT (tp.publicationcode = 'be/MMN')
-                AND NOT (tp.publicationcode = 'it/TL')
-                AND NOT (tp.publicationcode = 'se/WDS')
+                AND NOT (i.publicationcode = 'fr/JM' AND i.issuenumber REGEXP '^[0-9]+$')
+                AND NOT (i.publicationcode = 'be/MMN')
+                AND NOT (i.publicationcode = 'it/TL')
+                AND NOT (i.publicationcode = 'se/WDS')
               GROUP BY tp.ID) as edges_and_collaborators
         group by DATE_FORMAT(creationDate, '%Y-%m-%d %H:00:00'), edges_and_collaborators.users
     `) as EdgeCreationEventRaw[]
