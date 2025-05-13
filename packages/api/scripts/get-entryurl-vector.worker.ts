1d21d513cfb576680933076b54b7958967785cf4
Bruno Perel <brunoperel@gmail.com>
2025-05-13 17:27:08 +0200

    api: Use worker pool for calculating entryurl vectors

--- packages/api/scripts/get-entryurl-vector.worker.ts
+++ packages/api/scripts/get-entryurl-vector.worker.ts
@@ -1,12 +1,24 @@
-import { parentPort, workerData } from "worker_threads";
-import { getImageVector } from "../services/story-search";
+import { parentPort } from "worker_threads";
+import { getImageVector, loadModel } from "../services/story-search";
 
-(async () => {
+let modelLoaded = false;
+
+const processFile = async (filePath: string) => {
   try {
-    const vector = await getImageVector(workerData.filePath);
-    parentPort?.postMessage({ vector: vector.vector, filePath: workerData.filePath });
+    if (!modelLoaded) {
+      await loadModel();
+      modelLoaded = true;
+    }
+    const vector = await getImageVector(filePath);
+    parentPort?.postMessage({ type: "result", vector: vector.vector, filePath });
   } catch (error: unknown) {
     const message = typeof error === "object" && error && "message" in error ? (error as { message: string }).message : String(error);
-    parentPort?.postMessage({ error: message, filePath: workerData.filePath });
+    parentPort?.postMessage({ type: "error", error: message, filePath });
   }
-})(); 
+};
+
+parentPort?.on("message", (msg) => {
+  if (msg.type === "process" && msg.filePath) {
+    processFile(msg.filePath);
+  }
+}); 
