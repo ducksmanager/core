532d6efb1eb1fe028543d9a706255e64b109989d
Bruno Perel <brunoperel@gmail.com>
2025-09-06 21:29:40 +0200

    whattheduck: Fix camera preview not working

--- apps/whattheduck/src/components/CameraPreviewOverlay.vue
+++ apps/whattheduck/src/components/CameraPreviewOverlay.vue
@@ -1,10 +1,10 @@
 <template>
-  <div :id="cameraPreviewElementId"></div>
+  <div id="camera-preview" ref="cameraPreview"></div>
   <div ref="overlay" class="overlay">
     <ion-button ref="takePhotoButton" size="large" @click="takePhoto().then(() => (isCameraPreviewShown = false))">
       <ion-icon :ios="apertureOutline" :md="apertureSharp" />
     </ion-button>
-    <ion-button size="large" color="danger" @click="isCameraPreviewShown = false">
+    <ion-button size="large" color="danger" @click="CameraPreview.stop().finally(() => (isCameraPreviewShown = false))">
       <ion-icon :ios="closeOutline" :md="closeSharp" />
     </ion-button>
   </div>
@@ -12,6 +12,7 @@
 
 <script setup lang="ts">
 import { socketInjectionKey as dmSocketInjectionKey } from '~web/src/composables/useDmSocket';
+import { useElementSize } from '@vueuse/core';
 
 import { apertureOutline, apertureSharp, closeOutline, closeSharp } from 'ionicons/icons';
 import type { CameraPreviewOptions } from '@capacitor-community/camera-preview';
@@ -21,51 +22,38 @@ import useCoverSearch from '~/composables/useCoverSearch';
 import { app } from '~/stores/app';
 import { onIonViewWillLeave } from '@ionic/vue';
 
-const cameraPreviewElementId = 'camera-preview';
+const overlay = useTemplateRef<HTMLElement>('overlay');
+const { height: overlayHeight } = useElementSize(overlay);
+
+const cameraPreview = useTemplateRef<HTMLDivElement>('cameraPreview');
 
 const { coverId: coverIdEvents } = inject(dmSocketInjectionKey)!;
 const { takePhoto } = useCoverSearch(useRouter(), coverIdEvents);
 const { isCameraPreviewShown } = storeToRefs(app());
 
-const overlay = ref<HTMLElement>();
-const takePhotoButton = ref<{ $el: HTMLElement }>();
-
-watch(isCameraPreviewShown, async () => {
-  if (isCameraPreviewShown.value) {
-    let loopIteration = 0;
-    const interval = setInterval(() => {
-      const boundingClientRect = Object.entries(overlay.value!.getBoundingClientRect().toJSON()).reduce(
-        (acc, [key, value]) => ({
-          ...acc,
-          [key]: parseInt(
-            ((value as number) - (key === 'height' ? takePhotoButton.value!.$el.clientHeight : 0)).toFixed(),
-          ),
-        }),
-        {},
-      ) as DOMRect;
-
-      if (boundingClientRect.height) {
-        clearInterval(interval);
-        const cameraPreviewOptions: CameraPreviewOptions = {
-          parent: cameraPreviewElementId,
-          disableAudio: true,
-          position: 'rear',
-          ...boundingClientRect,
-        };
-        CameraPreview.start(cameraPreviewOptions);
-      } else if (loopIteration > 10) {
-        console.error('Could not get overlayElement height');
-        clearInterval(interval);
-      }
-      loopIteration++;
-    }, 50);
-  } else {
-    CameraPreview.stop();
-  }
-});
-
 onIonViewWillLeave(() => {
   isCameraPreviewShown.value = false;
+});
+
+watch(overlayHeight, async (overlayHeight) => {
+  if (overlayHeight) {
+    const boundingClientRect = Object.entries(cameraPreview.value!.getBoundingClientRect().toJSON()).reduce(
+      (acc, [key, value]) => ({
+        ...acc,
+        [key]: parseInt(((value as number) - (key === 'height' ? overlayHeight : 0)).toFixed()),
+      }),
+      {},
+    ) as DOMRect;
+    if (boundingClientRect.height) {
+      const cameraPreviewOptions: CameraPreviewOptions = {
+        parent: 'camera-preview',
+        disableAudio: true,
+        position: 'rear',
+        ...boundingClientRect,
+      };
+      await CameraPreview.start(cameraPreviewOptions);
+    }
+  }
 });
 </script>
 
@@ -83,6 +71,7 @@ onIonViewWillLeave(() => {
 
 #camera-preview {
   display: flex;
+  height: 100%;
 
   video {
     width: 100%;
