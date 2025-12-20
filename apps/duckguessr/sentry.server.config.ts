3e7cf079ad315d3310e0ff26cae9dddeab3f53b4
Bruno Perel <brunoperel@gmail.com>
2025-12-20 22:33:36 +0100

    Revert "duckguessr: Disable Sentry on local environments"
    This reverts commit 52b85756272fa661f71fcd1935d052b2732ae794.

--- apps/duckguessr/sentry.server.config.ts
+++ apps/duckguessr/sentry.server.config.ts
@@ -1,18 +1,15 @@
 import * as Sentry from "@sentry/nuxt";
 
-// Only initialize Sentry in production environments
-if (process.env.NODE_ENV === "production" && process.env.SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
 
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
 
-    // Enable logs to be sent to Sentry
-    enableLogs: true,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-  });
-}
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
