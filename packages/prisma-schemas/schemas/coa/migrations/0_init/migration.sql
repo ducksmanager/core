df50600b4c3a05595f53a8d7f2d70d16280a0cb2
Bruno Perel <brunoperel@gmail.com>
2026-07-04 13:24:30 +0200

    prisma-schemas: Add prisma migrate scripts

--- packages/prisma-schemas/schemas/coa/migrations/0_init/migration.sql
+++ packages/prisma-schemas/schemas/coa/migrations/0_init/migration.sql
@@ -1,12 +1,3 @@
-create table _prisma_migrations (
-    id varchar(36) not null primary key,
-    finished_at datetime(3) null,
-    migration_name varchar(255) not null,
-    logs text null,
-    rolled_back_at datetime(3) null,
-    started_at datetime(3) default current_timestamp(3) not null,
-    applied_steps_count int unsigned default 0 not null
-) collate = utf8mb4_unicode_ci;
 create table inducks_appearance (
     storyversioncode varchar(20) null,
     charactercode varchar(62) null,
--- packages/prisma-schemas/schemas/edgecreator/migrations/0_init/migration.sql
+++ packages/prisma-schemas/schemas/edgecreator/migrations/0_init/migration.sql
@@ -1,13 +1,3 @@
-create table _prisma_migrations (
-    id varchar(36) not null primary key,
-    checksum varchar(64) not null,
-    finished_at datetime(3) null,
-    migration_name varchar(255) not null,
-    logs text null,
-    rolled_back_at datetime(3) null,
-    started_at datetime(3) default current_timestamp(3) not null,
-    applied_steps_count int unsigned default 0 not null
-) collate = utf8mb4_unicode_ci;
 create table edgecreator_intervalles (
     ID int auto_increment primary key,
     ID_Valeur int(10) not null,
